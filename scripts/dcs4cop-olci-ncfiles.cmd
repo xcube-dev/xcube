176700b9932626f24455b1e2760bbb1fa8ac79e7
Norman Fomferra <norman.fomferra@gmail.com>
2018-05-29 18:24:15 +0200

    example scripts

---
+++ scripts/dcs4cop-olci-ncfiles.cmd
@@ -0,0 +1,8 @@
+reproj-snap-nc ^
+   -f nc ^
+   -s 2000,1000 ^
+   -r 0,50,5,52.5 ^
+   -v conc_chl,conc_tsm,kd489,c2rcc_flags,quality_flags ^
+   -d D:\EOData\DCS4COP ^
+   -n OLCI-SNS-RAW-CUBE ^
+   D:\EOData\HIGHROC\0001_SNS\OLCI\**\*.nc
---
+++ scripts/dcs4cop-olci-zarr.cmd
@@ -0,0 +1,9 @@
+reproj-snap-nc ^
+   -f zarr ^
+   -a ^
+   -s 2000,1000 ^
+   -r 0,50,5,52.5 ^
+   -v conc_chl,conc_tsm,kd489,c2rcc_flags,quality_flags ^
+   -d D:\EOData\DCS4COP ^
+   -n OLCI-SNS-RAW-CUBE-2 ^
+   D:\EOData\HIGHROC\0001_SNS\OLCI\**\*.nc
