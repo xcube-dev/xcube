e09386580a79f8c3fd7209817bd1f38a2312e64c
Norman Fomferra <norman.fomferra@gmail.com>
2018-05-30 18:00:06 +0200

    rename

---
+++ xcube/io.py
@@ -0,0 +1,19 @@
+import s3fs
+import xarray as xr
+import zarr
+
+
+def open_from_obs(path: str, endpoint_url: str = None, max_cache_size: int = 2 ** 28) -> xr.Dataset:
+    """
+    Open an xcube (xarray dataset) from S3 compatible object storage (OBS).
+    :param path: Path having format "<bucket>/<my>/<sub>/<path>"
+    :param endpoint_url: Optional URL of the OBS service endpoint. If omitted, AWS S3 service URL is used.
+    :param max_cache_size: If > 0, size of a memory cache in bytes.
+           If None or size <= 0, no memory cache will be used.
+    :return: an xarray
+    """
+    s3 = s3fs.S3FileSystem(anon=True, client_kwargs=dict(endpoint_url=endpoint_url))
+    store = s3fs.S3Map(root=path, s3=s3, check=False)
+    if max_cache_size is not None and max_cache_size > 0:
+        store = zarr.LRUStoreCache(store, max_size=max_cache_size)
+    return xr.open_zarr(store)
