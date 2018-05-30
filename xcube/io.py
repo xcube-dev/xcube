6bc8d390c3d17f063eaa537a48c28dd877aae95c
Norman Fomferra <norman.fomferra@gmail.com>
2018-05-30 18:47:46 +0200

    doc update

--- xcube/io.py
+++ xcube/io.py
@@ -8,9 +8,9 @@ def open_from_obs(path: str, endpoint_url: str = None, max_cache_size: int = 2 *
     Open an xcube (xarray dataset) from S3 compatible object storage (OBS).
     :param path: Path having format "<bucket>/<my>/<sub>/<path>"
     :param endpoint_url: Optional URL of the OBS service endpoint. If omitted, AWS S3 service URL is used.
-    :param max_cache_size: If > 0, size of a memory cache in bytes.
+    :param max_cache_size: If > 0, size of a memory cache in bytes, e.g. 2**30 = one giga bytes.
            If None or size <= 0, no memory cache will be used.
-    :return: an xarray
+    :return: an xarray dataset
     """
     s3 = s3fs.S3FileSystem(anon=True, client_kwargs=dict(endpoint_url=endpoint_url))
     store = s3fs.S3Map(root=path, s3=s3, check=False)
