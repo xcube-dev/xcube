9294dd387e040e57a5170de2600ab1468ce84585
Norman Fomferra <norman.fomferra@gmail.com>
2018-07-09 18:36:25 +0200

    updates

--- examples/cyanoalert-olci-de.cmd
+++ examples/cyanoalert-olci-de.cmd
@@ -1,7 +1,7 @@
 xcube-genl2c ^
-   -m D:\Projects\xcube\examples\cyanoalert-olci-config.yml ^
+   -c D:\Projects\xcube\examples\cyanoalert-olci-config.yml ^
    -f zarr ^
    -a ^
    -d D:\EOData\CyanoAlert ^
-   -n OLCI-DE-L2C-CUBE ^
+   -n OLCI-DE-L2C-CUBE-v2 ^
    D:\EOData\CyanoAlert\*.nc
