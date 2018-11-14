283cf9b9c8d55612494fa81aaeef2e6206fa3e34
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2018-11-14 11:52:09 +0100

    improved readability

--- docs/grid-resolutions.py
+++ docs/grid-resolutions.py
@@ -5,7 +5,8 @@ WGS84_ELLIPSOID_SEMI_MAJOR_AXIS = 6378137.
 EARTH_EQUATORIAL_PERIMETER = 2. * math.pi * WGS84_ELLIPSOID_SEMI_MAJOR_AXIS
 
 SEP = ";"
-H = 180
+T_MIN = 180
+L_MAX = 15
 
 
 def degrees_to_meters(res):
@@ -16,20 +17,20 @@ def res_table():
     table = []
 
     header = ["T", "N", "1/res0 (1/deg)", "res0 (deg)"]
-    for i in range(0, 16):
+    for i in range(0, L_MAX + 1):
         header.append(f"res{i}")
     table.append(tuple(header))
 
-    for t in range(H, 25 * H + 1):
-        if t > H and t % H == 0:
+    for t in range(T_MIN, 25 * T_MIN + 1):
+        if t > T_MIN and t % T_MIN == 0:
             continue
-        for n in range(0, 16):
+        for n in range(0, L_MAX + 1):
             u = t * 2 ** n
-            m = u // H
-            if H * m == u:
+            m = u // T_MIN
+            if T_MIN * m == u:
                 row = [t, n, m, 1 / m]
                 res0 = degrees_to_meters(1 / m)
-                for i in range(0, 16):
+                for i in range(0, L_MAX + 1):
                     row.append(res0 * math.pow(2, -i))
                 table.append(tuple(row))
                 break
