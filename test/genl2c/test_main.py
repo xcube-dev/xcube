b91cef01fccf9933ca55262cf39f53f4ec46dd9f
AliceBalfanz <alicja.balfanz@brockmann-consult.de>
2018-12-19 11:53:49 +0100

    added test for genl2c.main

--- test/genl2c/test_main.py
+++ test/genl2c/test_main.py
@@ -3,40 +3,16 @@ import unittest
 from xcube.genl2c.main import main
 
 
-# class CliTest(unittest.TestCase):
-#     def test_main_with_option_help(self):
-#         exit_code = main(['-h'])
-#         self.assertEqual(0, exit_code)
-#         exit_code = main(['--help'])
-#         self.assertEqual(0, exit_code)
-#
-#     def test_main_with_missing_args(self):
-#         exit_code = main([])
-#         # exit_code==0 when run from PyCharm, exit_code==2 else
-#         self.assertEqual(2, exit_code)
-#         #self.assertTrue(exit_code in [0, 2])
-#
-#     def test_main_with_illegal_size_option(self):
-#         exit_code = main(['-s', '120,abc', 'input.nc'])
-#         self.assertEqual(2, exit_code)
-#
-#     def test_main_with_illegal_region_option(self):
-#         exit_code = main(['-r', '50,_2,55,21', 'input.nc'])
-#         self.assertEqual(2, exit_code)
-#         exit_code = main(['-r', '50,20,55', 'input.nc'])
-#         self.assertEqual(2, exit_code)
-#
-#     def test_main_with_illegal_variable_option(self):
-#         exit_code = main(['-v', ' ', 'input.nc'])
-#         self.assertEqual(2, exit_code)
-#
-#     def test_main_with_illegal_config_option(self):
-#         exit_code = main(['-c', 'nonono.yml', 'input.nc'])
-#         self.assertEqual(2, exit_code)
-#
-#     def test_main_with_illegal_options(self):
-#         exit_code = main(['input.nc'])
-#         self.assertEqual(2, exit_code)
+class MainTest(unittest.TestCase):
+    def test_main_succeeds(self):
+        with self.assertRaises(SystemExit) as cm:
+            main(['--help'])
+        self.assertEqual("0", f"{cm.exception}")
+
+    def test_main_fails(self):
+        with self.assertRaises(SystemExit) as cm:
+            main(['--helpi'])
+        self.assertEqual("2", f"{cm.exception}")
 
 
 
