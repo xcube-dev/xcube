1ec66a98244939be838772774f1a02b87dc1a368
AliceBalfanz <alicja.balfanz@brockmann-consult.de>
2019-01-30 14:21:35 +0100

    Changing tests to click

--- test/genl2c/test_cli.py
+++ test/genl2c/test_cli.py
@@ -47,9 +47,19 @@ class CliTest(unittest.TestCase):
         result = self.invoke_cli(['input.nc'])
         self.assertEqual(2, result.exit_code)
 
+# This test still fails, needs to be fixed
     def test_version(self):
         result = self.invoke_cli(['--version'])
         print(result.output)
         print(result.exit_code)
         self.assertEqual(
-        f'{prog!r}, version 0.1.0.dev1', result.output)
+                         '  cli, version 0.1.0.dev1', result.output)
+
+# Keeping this test until the problem is isolved (issue #19 on GitHub) - it is commented out because of hardcoded paths
+#     def test_missing_output_name(self):
+#         result = self.invoke_cli(['--dir', '/home/alicja/Desktop/DCS4COP/Testing_naming_of_xcube_genl2/', '-f',
+#                                   'zarr', '--region', '4.0,48.0,5.333333333333333,49.33333333333333',
+#                                   '--size', '512,512', '--proc', 'default', '--append',
+#                                   '/home/alicja/Desktop/DCS4COP/CMEMS_SST_201701_coords_arranged/20170*.nc'])
+#         print(result.output)
+#         print(result.exit_code)
--- test/genl3/test_cli.py
+++ test/genl3/test_cli.py
@@ -1,32 +1,39 @@
 import unittest
+from typing import List
 
-from xcube.genl3.cli import main
+import click
+import click.testing
 
+from xcube.genl3.cli import cli
 
 class CliTest(unittest.TestCase):
+
+    @classmethod
+    def invoke_cli(cls, args: List[str]):
+        runner = click.testing.CliRunner()
+        return runner.invoke(cli, args)
+
     def test_help(self):
-        exit_code = main(['-h'])
-        self.assertEqual(0, exit_code)
-        exit_code = main(['--help'])
-        self.assertEqual(0, exit_code)
+        result = self.invoke_cli(['--help'])
+        self.assertEqual(0, result.exit_code)
 
     def test_missing_args(self):
-        exit_code = main([])
-        self.assertEqual(2, exit_code)
+        result = self.invoke_cli([])
+        self.assertEqual(2, result.exit_code)
 
     def test_variable_option(self):
-        exit_code = main(['-v', ' ', 'input.nc'])
-        self.assertEqual(2, exit_code)
+        result = self.invoke_cli(['-v', ' ', 'input.nc'])
+        self.assertEqual(2, result.exit_code)
 
     def test_metadata_option(self):
-        exit_code = main(['-m', 'nonono.yml', 'input.nc'])
-        self.assertEqual(2, exit_code)
+        result = self.invoke_cli(['-m', 'nonono.yml', 'input.nc'])
+        self.assertEqual(2, result.exit_code)
 
     def test_resampling_option(self):
-        exit_code = main(['--resampling', 'NeArEsT'])
-        self.assertEqual(2, exit_code)
+        result = self.invoke_cli(['--resampling', 'NeArEsT'])
+        self.assertEqual(2, result.exit_code)
 
     def test_illegal_input(self):
-        exit_code = main(['input.nc'])
-        self.assertEqual(2, exit_code)
+        result = self.invoke_cli(['input.nc'])
+        self.assertEqual(2, result.exit_code)
 
