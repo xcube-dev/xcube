e840fbc5e950be59114c3c92b07e735de2132af9
Norman Fomferra <norman.fomferra@gmail.com>
2019-02-05 17:12:58 +0100

    removed old "grid" tool

--- setup.py
+++ setup.py
@@ -57,7 +57,6 @@ setup(
             'xcube = xcube.cli.cli:main',
             'xcube-genl2c = xcube.genl2c.cli:main',
             'xcube-genl3 = xcube.genl3.cli:main',
-            'xcube-grid = xcube.grid.main:main',
         ],
         'xcube_plugins': [
             'xcube_genl2c_default = xcube.genl2c.default:init_plugin',
