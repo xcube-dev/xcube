81a3bfbaf3068b736109d64cba7e0f896a8f00ac
Norman Fomferra <norman.fomferra@gmail.com>
2019-02-05 11:02:45 +0100

    integrated py3.7

--- test/test_cli.py
+++ test/test_cli.py
@@ -39,18 +39,30 @@ class DumpTest(CliTest):
     def test_dump_ds(self):
         result = self.invoke_cli(["dump", TEST_NC_FILE])
         self.assertEqual((
-            '<xarray.Dataset>\n'
-            'Dimensions:        (lat: 100, lon: 200, time: 5)\n'
-            'Coordinates:\n'
-            '  * time           (time) datetime64[ns] 2010-01-01 2010-01-02 ... 2010-01-05\n'
-            '  * lat            (lat) float64 50.0 50.02 50.04 50.06 ... 51.96 51.98 52.0\n'
-            '  * lon            (lon) float64 0.0 0.0201 0.0402 0.0603 ... 3.94 3.96 3.98 4.0\n'
-            'Data variables:\n'
-            '    precipitation  (time, lat, lon) float64 ...\n'
-            '    temperature    (time, lat, lon) float64 ...\n'
-            'Attributes:\n'
-            '    time_coverage_start:  2010-01-01 00:00:00\n'
-            '    time_coverage_end:    2010-01-05 00:00:00\n'
+            "<xarray.Dataset>\n"
+            "Dimensions:        (bnds: 2, lat: 180, lon: 360, time: 5)\n"
+            "Coordinates:\n"
+            "  * lon            (lon) float64 -179.5 -178.5 -177.5 ... 177.5 178.5 179.5\n"
+            "  * lat            (lat) float64 -89.5 -88.5 -87.5 -86.5 ... 86.5 87.5 88.5 89.5\n"
+            "  * time           (time) datetime64[ns] 2010-01-01T12:00:00 ... 2010-01-05T12:00:00\n"
+            "    lon_bnds       (lon, bnds) float64 ...\n"
+            "    lat_bnds       (lat, bnds) float64 ...\n"
+            "    time_bnds      (time, bnds) datetime64[ns] ...\n"
+            "Dimensions without coordinates: bnds\n"
+            "Data variables:\n"
+            "    precipitation  (time, lat, lon) float64 ...\n"
+            "    temperature    (time, lat, lon) float64 ...\n"
+            "Attributes:\n"
+            "    Conventions:           CF-1.7\n"
+            "    title:                 Test Cube\n"
+            "    time_coverage_start:   2010-01-01 00:00:00\n"
+            "    time_coverage_end:     2010-01-06 00:00:00\n"
+            "    geospatial_lon_min:    -180.0\n"
+            "    geospatial_lon_max:    180.0\n"
+            "    geospatial_lon_units:  degrees_east\n"
+            "    geospatial_lat_min:    -90.0\n"
+            "    geospatial_lat_max:    90.0\n"
+            "    geospatial_lat_units:  degrees_north\n"
         ), result.output)
         self.assertEqual(0, result.exit_code)
 
@@ -149,6 +161,3 @@ class ParseTest(unittest.TestCase):
             _parse_kwargs("9==2")
         self.assertEqual("Invalid value: '9==2'",
                          f"{cm.exception}")
-
-
-
