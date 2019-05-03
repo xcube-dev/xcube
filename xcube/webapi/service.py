e9a59264d3d277b71f2d0e2536198879b4c2b2aa
Norman Fomferra <norman.fomferra@gmail.com>
2019-05-03 09:31:41 +0200

    trying to fix CORS problem

--- xcube/webapi/service.py
+++ xcube/webapi/service.py
@@ -220,9 +220,11 @@ class ServiceRequestHandler(RequestHandler):
         return self._params
 
     def set_default_headers(self):
-        self.set_header("Access-Control-Allow-Origin", "*")
-        self.set_header("Access-Control-Allow-Headers", "x-requested-with")
-        self.set_header('Access-Control-Allow-Methods', 'GET, PUT, DELETE, OPTIONS')
+        self.set_header('Access-Control-Allow-Origin', '*')
+        self.set_header('Access-Control-Allow-Methods',
+                        'GET,PUT,DELETE,OPTIONS')
+        self.set_header('Access-Control-Allow-Headers',
+                        'x-requested-with,access-control-allow-origin,authorization,content-type')
 
     def options(self):
         self.set_status(204)
