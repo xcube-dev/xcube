a524d3061e7e79a98625146cdb41511af283c5be
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2019-05-17 14:23:48 +0200

    added `--show` option to `xcube serve` which will open the viewer

--- test/cli/test_serve.py
+++ test/cli/test_serve.py
@@ -1,9 +1,41 @@
-import unittest
-
+from xcube.cli.serve import VIEWER_ENV_VAR
 from xcube.cli.serve import main
+from .test_cli import CliTest
 
 
-class ServerCliSmokeTest(unittest.TestCase):
+class ServerCliTest(CliTest):
+    def test_config(self):
+        result = self.invoke_cli(["serve", "--config", "x.yml", "pippo.zarr"])
+        self.assertEqual(1, result.exit_code)
+        self.assertEqual('Error: CONFIG and CUBES cannot be used at the same time.\n',
+                         result.output[-58:])
+
+        result = self.invoke_cli(["serve", "--config", "x.yml", "pippo.zarr"])
+        self.assertEqual(1, result.exit_code)
+        self.assertEqual('Error: CONFIG and CUBES cannot be used at the same time.\n',
+                         result.output[-58:])
+
+    def test_show(self):
+        import os
+        viewer_path = os.environ.get(VIEWER_ENV_VAR)
+
+        del os.environ[VIEWER_ENV_VAR]
+        result = self.invoke_cli(["serve", "--show"])
+        self.assertEqual(2, result.exit_code)
+        self.assertEqual(f'Error: Option "--show" / "-s": In order to run the viewer, '
+                         f'set environment variable {VIEWER_ENV_VAR} to a valid '
+                         f'xcube-viewer deployment or build directory.\n',
+                         result.output[-157:])
+
+        os.environ[VIEWER_ENV_VAR] = "pip/po"
+        result = self.invoke_cli(["serve", "--show"])
+        self.assertEqual(2, result.exit_code)
+        self.assertEqual(f'Error: Option "--show" / "-s": Viewer path set by environment '
+                         f'variable {VIEWER_ENV_VAR} '
+                         f'must be a directory: pip/po\n',
+                         result.output[-117:])
+
+        os.environ[VIEWER_ENV_VAR] = viewer_path
 
     def test_help(self):
         with self.assertRaises(SystemExit):
