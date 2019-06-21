988b5555facb8539e4338313f1830fda1c80fb00
Norman Fomferra <norman.fomferra@gmail.com>
2019-06-21 18:00:06 +0200

    started impl. pruning

--- xcube/api/__init__.py
+++ xcube/api/__init__.py
@@ -7,6 +7,7 @@ from .levels import compute_levels, read_levels, write_levels
 from .new import new_cube
 from .readwrite import read_cube, open_cube, open_dataset, read_dataset, write_dataset
 from .resample import resample_in_time
+from .select import select_vars
 from .ts import get_time_series
 from .vars_to_dim import vars_to_dim
 from .verify import assert_cube, verify_cube
