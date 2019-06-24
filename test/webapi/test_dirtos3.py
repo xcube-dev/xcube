deb0fa1a373003dd46a360abdff20e15b08176ce
Norman Fomferra <norman.fomferra@gmail.com>
2019-06-24 17:37:34 +0200

    started representing a dir as S3 bucket

--- test/webapi/test_dirtos3.py
+++ test/webapi/test_dirtos3.py
@@ -1,5 +1,5 @@
-import unittest
 import json
+import unittest
 
 from xcube.api import new_cube
 from xcube.api.readwrite import write_cube
@@ -10,7 +10,7 @@ class DirToS3Test(unittest.TestCase):
 
     def test_it(self):
         self.maxDiff = None
-        result = dir_to_s3(self.TEST_CUBE, '')
+        result = dir_to_s3(self.TEST_CUBE, self.TEST_CUBE, '')
         print(json.dumps(result, indent=2))
         self.assertIsInstance(result, dict)
         self.assertIn('ListBucketResult', result)
@@ -37,38 +37,55 @@ class DirToS3Test(unittest.TestCase):
 import os.path
 import datetime
 
-def dir_to_s3(dir_path, rel_path, delimiter='/', prefix=None, max_keys=1000, marker=None):
+
+def dir_to_s3(bucket_name, dir_path, rel_path, delimiter='/', prefix=None, max_keys=1000, marker=None):
     full_path = os.path.abspath(os.path.normpath(os.path.join(dir_path, rel_path)))
     if os.path.isfile(full_path):
         # Get object
         return None
     else:
-        i = 0
         contents_list = []
+        is_truncated = False
+        next_marker = None
+        marker_seen = marker is None
+
         for root, dirs, files in os.walk(full_path):
             common_prefix = os.path.commonprefix([full_path, root])
             key_prefix = root[len(common_prefix) + 1:].replace('\\', '/') + '/'
 
-            key = key_prefix
-            stat = os.stat(os.path.join(root))
-            contents_list.append(dict(Key=key,
-                                      Size=stat.st_size,
-                                      LastModified=str(datetime.datetime.fromtimestamp(stat.st_mtime)),
-                                      ETag=hex(abs(hash(root + key)))[2:],
-                                      StorageClass='STANDARD'))
-            if i >= max_keys:
-                break
-            i += 1
+            keys_and_paths = [(key_prefix, root)]
             for file in files:
-                stat = os.stat(os.path.join(root, file))
-                key = key_prefix + file
+                keys_and_paths.append((key_prefix + file, os.path.join(root, file)))
+
+            for key, path in keys_and_paths:
+
+                if key == marker:
+                    marker_seen = True
+
+                if not marker_seen:
+                    continue
+
+                if len(contents_list) == max_keys:
+                    is_truncated = True
+                    next_marker = key
+                    break
+
+                stat = os.stat(os.path.join(root))
                 contents_list.append(dict(Key=key,
                                           Size=stat.st_size,
                                           LastModified=str(datetime.datetime.fromtimestamp(stat.st_mtime)),
                                           ETag=hex(abs(hash(root + key)))[2:],
                                           StorageClass='STANDARD'))
-                if i >= max_keys:
-                    break
-                i += 1
 
-        return dict(ListBucketResult=dict(Contents=contents_list))
+        return dict(
+            ListBucketResult=dict(
+                Name=bucket_name,
+                Prefix=prefix,
+                Delimiter=delimiter,
+                MaxKeys=max_keys,
+                IsTruncated=is_truncated,
+                Marker=marker,
+                NextMarker=next_marker,
+                Contents=contents_list
+            )
+        )
