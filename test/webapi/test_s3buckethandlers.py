fec856e5791940b36bfa04be5a1eab42b6b46dcf
Norman Fomferra <norman.fomferra@gmail.com>
2019-06-26 17:34:56 +0200

    extended test

--- test/webapi/test_s3buckethandlers.py
+++ test/webapi/test_s3buckethandlers.py
@@ -1,10 +1,12 @@
 import unittest
 import urllib.request
 
+import numpy as np
+
 from xcube.api import read_cube
 
-SKIP_HELP = 'Require: xcube serve -p 9999 -c xcube/webapi/res/demo/cube.zarr'
-SERVER_URL = 'http://localhost:9999'
+SKIP_HELP = 'Require: xcube serve -c xcube/webapi/res/demo/cube.zarr'
+SERVER_URL = 'http://localhost:8080'
 ENDPOINT_URL = SERVER_URL + '/s3bucket'
 
 
@@ -15,7 +17,7 @@ def is_server_running() -> bool:
             response.read()
     except Exception:
         return False
-    return 200 <= response.status_code < 400
+    return 200 <= response.code < 400
 
 
 XCUBE_SERVER_IS_RUNNING = is_server_running()
@@ -27,8 +29,14 @@ class S3BucketHandlersTest(unittest.TestCase):
     def test_read_cube_from_xube_server(self):
         ds = read_cube('s3bucket/local.zarr', format_name='zarr', endpoint_url=SERVER_URL)
         self.assertIsNotNone(ds)
+        self.assertEqual((5, 1000, 2000), ds.conc_chl.shape)
+        self.assertEqual(('time', 'lat', 'lon'), ds.conc_chl.dims)
+        conc_chl_values = ds.conc_chl.values
+        self.assertEqual((5, 1000, 2000), conc_chl_values.shape)
+        self.assertAlmostEqual(0.00005656, float(np.nanmin(conc_chl_values)), delta=1e-6)
+        self.assertAlmostEqual(22.4421215, float(np.nanmax(conc_chl_values)), delta=1e-6)
 
-    def test_read_cube_from_obs(self):
-        ds = read_cube('dcs4cop-obs-01/OLCI-SNS-RAW-CUBE-2.zarr', format_name='zarr',
-                       endpoint_url='https://obs.eu-de.otc.t-systems.com')
-        self.assertIsNotNone(ds)
+    # def test_read_cube_from_obs(self):
+    #     ds = read_cube('dcs4cop-obs-01/OLCI-SNS-RAW-CUBE-2.zarr', format_name='zarr',
+    #                    endpoint_url='https://obs.eu-de.otc.t-systems.com')
+    #     self.assertIsNotNone(ds)
