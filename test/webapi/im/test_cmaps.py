3e7e15a34e3563d10bf9606dcfb036a97119f3e0
AliceBalfanz <alicja.balfanz@brockmann-consult.de>
2019-07-09 14:34:22 +0200

    fixed failing test.

--- test/webapi/im/test_cmaps.py
+++ test/webapi/im/test_cmaps.py
@@ -68,21 +68,21 @@ class CmapsTest(TestCase):
 
     def test_get_cmaps_registers_snap_color(self):
         ensure_cmaps_loaded()
-        cmap_name = 'chl_DeM2_200.cpd'
+        cmap_name = 'webapi/im/chl_DeM2_200.cpd'
         cmap = _get_custom_colormap(cmap_name)
         cm.register_cmap(cmap=cmap)
         self.assertTrue((type(cmap) is LinearSegmentedColormap) or (type(cmap) is ListedColormap))
 
     def test_get_cmaps_registers_ivalid_snap_color(self):
         ensure_cmaps_loaded()
-        cmap_name = 'chl_DeM2_200_invalid_for_testing.cpd'
+        cmap_name = 'webapi/im/chl_DeM2_200_invalid_for_testing.cpd'
         with self.assertRaises(ValueError):
             cmap = _get_custom_colormap(cmap_name)
             cm.register_cmap(cmap=cmap)
 
     def test_get_cmaps_registers_nonexisting_snap_color(self):
         ensure_cmaps_loaded()
-        cmap_name = 'chl_DeM2_200_not_existing.cpd'
+        cmap_name = 'webapi/im/chl_DeM2_200_not_existing.cpd'
         with self.assertRaises(ValueError):
             cmap = _get_custom_colormap(cmap_name)
             cm.register_cmap(cmap=cmap)
