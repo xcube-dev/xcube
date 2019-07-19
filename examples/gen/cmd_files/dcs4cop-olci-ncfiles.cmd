a4f8e574e20a599dacf68789906dd949a269ba5d
AliceBalfanz <alicja.balfanz@brockmann-consult.de>
2019-07-19 15:13:04 +0200

    removing cmd_files directory, because it contains paths to our internal EOData archive.

--- examples/gen/cmd_files/dcs4cop-olci-ncfiles.cmd
+++
@@ -1,8 +0,0 @@
-xcube-genl2c ^
-   -p snap-olci-highroc-l2 ^
-   -w netcdf4 ^
-   -s 2000,1000 ^
-   -r 0,50,5,52.5 ^
-   -d D:\EOData\DCS4COP ^
-   -n OLCI-SNS-RAW-CUBE ^
-   D:\EOData\HIGHROC\0001_SNS\OLCI\**\*.nc
