ff4a18db5ef19360371306c027cd1554babc1773
AliceBalfanz <alicja.balfanz@brockmann-consult.de>
2019-07-30 18:14:58 +0200

    removing zarrinsert from utils and moving it into xcube gen module

--- xcube/api/gen/gen.py
+++ xcube/api/gen/gen.py
@@ -22,11 +22,15 @@
 import cProfile
 import glob
 import io
+import json
+import numpy as np
 import os
 import pstats
+import shutil
 import tempfile
 import time
 import traceback
+import xarray as xr
 from typing import Sequence, Callable, Tuple, Dict, Any
 
 from .defaults import DEFAULT_OUTPUT_SIZE, DEFAULT_OUTPUT_RESAMPLING, DEFAULT_OUTPUT_PATH
@@ -36,8 +40,7 @@ from ..select import select_vars
 from ..update import update_var_props, update_global_attrs
 from ...util.config import NameAnyDict, NameDictPairList, to_resolved_name_dict_pairs
 from ...util.dsio import rimraf, DatasetIO, find_dataset_io, guess_dataset_format
-from ...util.timecoord import add_time_coords
-from ...util.zarrinsert import check_append_or_insert, insert_input_file_into_output_path
+from ...util.timecoord import add_time_coords, get_time_in_days_since_1970
 
 _PROFILING_ON = False
 
@@ -189,6 +192,7 @@ def _get_half_time(time_range: Tuple[float, float]):
     halftime = start_time + half_seconds
     return halftime
 
+
 def _process_input(input_processor: InputProcessor,
                    input_reader: DatasetIO,
                    input_reader_params: Dict[str, Any],
@@ -294,7 +298,7 @@ def _process_input(input_processor: InputProcessor,
                 steps.append((step9, f'appending to {output_path}'))
 
             elif output_path.endswith('.zarr'):
-                append_input_file_to_output_path = check_append_or_insert(time_range, output_path)
+                append_input_file_to_output_path = _check_append_or_insert(time_range, output_path)
                 if append_input_file_to_output_path:
                     def step9(dataset):
                         output_writer.append(dataset, output_path, **output_writer_params)
@@ -306,7 +310,7 @@ def _process_input(input_processor: InputProcessor,
                     def step9(dataset):
                         input_tempdir = tempfile.TemporaryDirectory()
                         output_writer.write(dataset, input_tempdir.name, **output_writer_params)
-                        merged_data_set = insert_input_file_into_output_path(input_tempdir.name, output_path)
+                        merged_data_set = _insert_input_file_into_output_path(input_tempdir.name, output_path)
                         if not merged_data_set:
                             monitor('Time stamp of input data set is already exists in output: skipping it...')
                             return False
@@ -356,3 +360,91 @@ def _process_input(input_processor: InputProcessor,
         print(s.getvalue())
 
     return True
+
+
+def _check_append_or_insert(time_range: Tuple[float, float], output_path: str) -> bool:
+    """Checking if time stamp needs to be inserted into output path or appended."""
+    t1, t2 = time_range
+    if t1 != t2:
+        t_center = (t1 + t2) / 2
+    else:
+        t_center = t1
+    with xr.open_zarr(output_path) as ds:
+        return np.greater(t_center, get_time_in_days_since_1970(ds.time.values[-1]))
+
+
+def _insert_input_file_into_output_path(input_path: str, output_path: str) -> bool:
+    """Merging the data for the new time stamp into the existing and remaining zarr directory."""
+    insert = _check_if_insert_into_output_path(input_path, output_path)
+    if insert:
+        with xr.open_zarr(input_path) as input_ds, xr.open_zarr(output_path) as output_ds:
+            input_time_idx = 0
+            new_time_idx = np.amin(np.where(output_ds.time[:] > (input_ds.time.values[input_time_idx])))
+            # Preparing the source directory with the single time stamp to be ready for merging
+            # --> files of variables, excluding "lat" and "lon" need to be renamed
+            _rename_file(input_path, input_time_idx, new_time_idx)
+            # Preparing the destination directory to be ready for single time stam to be merged
+            # --> files of variables, excluding "lat" and "lon" need to be renamed
+            # The renaming needs to happen in reversed order and starting at the index of nearest above value:
+            for i in reversed(range(new_time_idx, output_ds.time.shape[0])):
+                _rename_file(output_path, i, (i + 1))
+            # Final step: copy the single time stamp files into destination zarr and adjusting .zarray to the change.
+            _copy_into_output_path(input_path, output_path, new_time_idx)
+            return True
+    else:
+        return False
+
+
+def _check_if_insert_into_output_path(input_path: str, output_path: str) -> bool:
+    """Check if to be added time stamp is unique """
+    with xr.open_zarr(input_path) as input_ds, xr.open_zarr(output_path) as output_ds:
+        mask = np.equal(output_ds.time.values, input_ds.time.values)
+        return not mask.any()
+
+
+def _rename_file(path: str, old_time_idx: int, new_time_idx: int):
+    """Renaming files within the directories according to new time index."""
+    for variable in _get_variables_and_dims_to_rename_and_copy(path):
+        v_path = os.path.join(path, variable)
+        for root, dirs, files in os.walk(v_path):
+            for filename in files:
+                parts1 = filename.split('.', 1)[0]
+                if parts1 == (str(old_time_idx)) and (variable != "time"):
+                    parts2 = filename.split('.', 1)[1]
+                    new_name = f'{new_time_idx}.{parts2}'
+                    os.rename(os.path.join(v_path, filename), os.path.join(v_path, new_name))
+                elif parts1 == (str(old_time_idx)) and (variable == "time"):
+                    os.rename(os.path.join(v_path, filename), os.path.join(v_path, str(new_time_idx)))
+
+
+def _copy_into_output_path(input_path: str, output_path: str, input_time_idx: int):
+    """Copy the files with the new time stamp into the existing zarr directory."""
+    for variable in _get_variables_and_dims_to_rename_and_copy(input_path):
+        v_path = os.path.join(input_path, variable)
+        for root, dirs, files in os.walk(v_path):
+            for filename in files:
+                parts1 = filename.split('.', 1)[0]
+                if parts1 == str(input_time_idx):
+                    shutil.copyfile((os.path.join(input_path, variable, filename)),
+                                    (os.path.join(output_path, variable, filename)))
+        _adjust_zarray(output_path, variable)
+
+
+def _get_variables_and_dims_to_rename_and_copy(path: str):
+    with xr.open_zarr(path) as ds:
+        variables = []
+        for v in ds.variables:
+            if 'time' in ds[v].dims:
+                variables.append(v)
+    return variables
+
+
+def _adjust_zarray(output_path: str, variable: str):
+    """Changing the shape for time in the .zarray file."""
+    with open((os.path.join(output_path, variable, '.zarray')), 'r') as jsonFile:
+        data = json.load(jsonFile)
+    t_shape = data["shape"]
+    data["shape"][0] = t_shape[0] + 1
+
+    with open((os.path.join(output_path, variable, '.zarray')), 'w') as jsonFile:
+        json.dump(data, jsonFile, indent=4)
