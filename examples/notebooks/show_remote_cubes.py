6182f8ab9e45bdee2095f903a4c47d5459781e80
AliceBalfanz <alicja.balfanz@brockmann-consult.de>
2019-09-13 16:00:10 +0200

    Reorganising notebook folder and deviding notebook in smaller ones

---
+++ examples/notebooks/show_remote_cubes.py
@@ -0,0 +1,34 @@
+import os
+import pandas as pd
+import s3fs
+from xcube.api import read_cube
+
+def show_remote_cubes():
+    path = 'dcs4cop-obs-01/'
+    s3_client_kwargs = {}
+    s3_client_kwargs['endpoint_url'] = 'http://obs.eu-de.otc.t-systems.com'
+    s3_client_kwargs['region_name'] = 'eu-de'
+    obs_file_system = s3fs.S3FileSystem(anon=True, client_kwargs=s3_client_kwargs)
+
+    cube_names = []
+    df = pd.DataFrame(columns=['cube_name', 'chunksize (time, lat, lon)', 'var_nums', 'variables', 'start_date', 'end_date'])
+    total_size = 0
+    for filepath in sorted(obs_file_system.ls(path)):
+        if filepath.endswith('.zarr'):
+            ds = read_cube(os.path.join('http://obs.eu-de.otc.t-systems.com',filepath))
+            var_list = []
+            for v in ds.variables:
+                if v not in ds.dims and 'bnds' not in v: 
+                    var_list.append(v)
+            cube_names.append(filepath)
+            filename = filepath.split('/')[1]
+            chunking = ds[var_list[0]].shape
+            sd= pd.to_datetime(str(ds.time.values[0])) 
+            start_date = sd.strftime('%Y-%m-%d')
+            ed= pd.to_datetime(str(ds.time.values[-1])) 
+            end_date = ed.strftime('%Y-%m-%d')
+            chunksize = ds[var_list[0]].data.chunksize
+            df = df.append({'cube_name': filename, 'chunksize (time, lat, lon)': chunksize, 
+                            'var_nums':len(var_list), 'variables':(str(var_list)).replace('[','').replace(']','').replace("'",""), 
+                            'start_date':start_date, 'end_date':end_date}, ignore_index=True)
+    return df
