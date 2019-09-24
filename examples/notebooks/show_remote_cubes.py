531fe3d48123cefc824cb3ad59aee9db717a8f53
Norman Fomferra <norman.fomferra@gmail.com>
2019-09-24 16:49:43 +0200

    updates

--- examples/notebooks/show_remote_cubes.py
+++ examples/notebooks/show_remote_cubes.py
@@ -19,11 +19,9 @@ def show_remote_cubes(bucket, endpoint_url, region_name):
 
     for filepath in sorted(obs_file_system.ls(bucket)):
         if filepath.endswith('.zarr'):
-            ds = read_cube(os.path.join('http://obs.eu-de.otc.t-systems.com', filepath))
-            var_list = []
-            for v in ds.variables:
-                if v not in ds.dims and 'bnds' not in v:
-                    var_list.append(v)
+            # ds = read_cube(f'{endpoint_url}/{filepath}', consolidated=True)
+            ds = read_cube(f'{endpoint_url}/{filepath}')
+            var_list = list(ds.data_vars)
             cube_names.append(filepath)
             filename = filepath.split('/')[1]
             sd = pd.to_datetime(str(ds.time.values[0]))
@@ -31,8 +29,10 @@ def show_remote_cubes(bucket, endpoint_url, region_name):
             ed = pd.to_datetime(str(ds.time.values[-1]))
             end_date = ed.strftime('%Y-%m-%d')
             chunksize = ds[var_list[0]].data.chunksize
-            df = df.append({'cube_name': filename, 'chunksize (time, lat, lon)': chunksize,
-                            'var_nums': len(var_list),
+            df = df.append({'cube_name': filename,
+                            'chunks (time, lat, lon)': chunksize,
+                            'num_vars': len(var_list),
                             'variables': (str(var_list)).replace('[', '').replace(']', '').replace("'", ""),
-                            'start_date': start_date, 'end_date': end_date}, ignore_index=True)
+                            'start_date': start_date,
+                            'end_date': end_date}, ignore_index=True)
     return df
