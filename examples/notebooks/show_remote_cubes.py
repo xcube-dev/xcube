b424975600c57f88c1f8cf1f2a1958d22ff98209
Norman Fomferra <norman.fomferra@gmail.com>
2019-11-05 17:18:38 +0100

    updates

--- examples/notebooks/show_remote_cubes.py
+++ examples/notebooks/show_remote_cubes.py
@@ -3,7 +3,7 @@ import os
 import pandas as pd
 import s3fs
 
-from xcube.core import read_cube
+from xcube.core import open_cube
 
 
 def show_remote_cubes(bucket, endpoint_url, region_name):
@@ -20,19 +20,19 @@ def show_remote_cubes(bucket, endpoint_url, region_name):
     for filepath in sorted(obs_file_system.ls(bucket)):
         if filepath.endswith('.zarr'):
             # ds = read_cube(f'{endpoint_url}/{filepath}', consolidated=True)
-            ds = read_cube(f'{endpoint_url}/{filepath}')
-            var_list = list(ds.data_vars)
-            cube_names.append(filepath)
-            filename = filepath.split('/')[1]
-            sd = pd.to_datetime(str(ds.time.values[0]))
-            start_date = sd.strftime('%Y-%m-%d')
-            ed = pd.to_datetime(str(ds.time.values[-1]))
-            end_date = ed.strftime('%Y-%m-%d')
-            chunksize = ds[var_list[0]].data.chunksize
-            df = df.append({'cube_name': filename,
-                            'chunks (time, lat, lon)': chunksize,
-                            'num_vars': len(var_list),
-                            'variables': (str(var_list)).replace('[', '').replace(']', '').replace("'", ""),
-                            'start_date': start_date,
-                            'end_date': end_date}, ignore_index=True)
+            with open_cube(f'{endpoint_url}/{filepath}') as ds:
+                var_list = list(ds.data_vars)
+                cube_names.append(filepath)
+                filename = filepath.split('/')[1]
+                sd = pd.to_datetime(str(ds.time.values[0]))
+                start_date = sd.strftime('%Y-%m-%d')
+                ed = pd.to_datetime(str(ds.time.values[-1]))
+                end_date = ed.strftime('%Y-%m-%d')
+                chunksize = ds[var_list[0]].data.chunksize
+                df = df.append({'cube_name': filename,
+                                'chunks (time, lat, lon)': chunksize,
+                                'num_vars': len(var_list),
+                                'variables': (str(var_list)).replace('[', '').replace(']', '').replace("'", ""),
+                                'start_date': start_date,
+                                'end_date': end_date}, ignore_index=True)
     return df
