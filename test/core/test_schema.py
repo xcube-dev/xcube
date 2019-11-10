323cc159d7862a824e094af5926fe972f3063f6f
Norman Fomferra <norman.fomferra@gmail.com>
2019-11-10 14:06:56 +0100

    Added tests for CubeSchema Added xarray accessor `xcube.schema` with HTML representation

---
+++ test/core/test_schema.py
@@ -0,0 +1,143 @@
+import unittest
+
+import numpy as np
+import xarray as xr
+
+from xcube.core.new import new_cube
+from xcube.core.schema import CubeSchema
+
+
+class CubeSchemaTest(unittest.TestCase):
+
+    def test_new(self):
+        cube = new_cube(variables=dict(a=2, b=3, c=4))
+        schema = CubeSchema.new(cube)
+        self._assert_schema(schema, expected_shape=cube.a.shape)
+
+    def test_new_chunked(self):
+        cube = new_cube(variables=dict(a=2, b=3, c=4))
+        cube = cube.chunk(dict(time=1, lat=90, lon=90))
+        schema = CubeSchema.new(cube)
+        self._assert_schema(schema, expected_shape=cube.a.shape, expected_chunks=(1, 90, 90))
+
+    def test_xarray_accessor(self):
+        # noinspection PyUnresolvedReferences
+        import xcube.core.xarray
+        cube = new_cube(variables=dict(a=2, b=3, c=4))
+        schema = cube.xcube.schema
+        self._assert_schema(schema, expected_shape=cube.a.shape)
+
+    def _assert_schema(self, schema: CubeSchema, expected_shape=None, expected_chunks=None):
+        self.assertEqual(3, schema.ndim)
+        self.assertEqual(expected_shape, schema.shape)
+        self.assertEqual(expected_chunks, schema.chunks)
+        self.assertIn('time', schema.coords)
+        self.assertIn('lat', schema.coords)
+        self.assertIn('lon', schema.coords)
+        self.assertEqual(('time', 'lat', 'lon'), schema.dims)
+        self.assertEqual('time', schema.time_dim)
+        self.assertEqual(('lat', 'lon'), schema.spatial_dims)
+
+    def test_repr_html(self):
+        cube = new_cube(variables=dict(a=2, b=3, c=4))
+        cube = cube.chunk(dict(time=1, lat=90, lon=90))
+        schema = CubeSchema.new(cube)
+        self.assertEqual("<table>"
+                         "<tr><td>Shape:</td><td>(5, 180, 360)</td></tr>"
+                         "<tr><td>Chunk sizes:</td><td>(1, 90, 90)</td></tr>"
+                         "<tr><td>Dimensions:</td><td>('time', 'lat', 'lon')</td></tr>"
+                         "</table>",
+                         schema._repr_html_())
+
+    def test_constructor_with_invalid_args(self):
+        cube = new_cube(variables=dict(t=273))
+        schema = CubeSchema.new(cube)
+        with self.assertRaises(ValueError) as cm:
+            # noinspection PyTypeChecker
+            CubeSchema(None, schema.coords)
+        self.assertEqual('shape must be a sequence of integer sizes',
+                         f'{cm.exception}')
+
+        with self.assertRaises(ValueError) as cm:
+            # noinspection PyTypeChecker
+            CubeSchema(schema.shape, None)
+        self.assertEqual('coords must be a mapping from dimension names to label arrays',
+                         f'{cm.exception}')
+
+        with self.assertRaises(ValueError) as cm:
+            CubeSchema(schema.shape[1:], schema.coords)
+        self.assertEqual('shape must have at least three dimensions',
+                         f'{cm.exception}')
+
+        with self.assertRaises(ValueError) as cm:
+            CubeSchema(schema.shape, schema.coords, dims=('lat', 'lon'))
+        self.assertEqual('dims must have same length as shape',
+                         f'{cm.exception}')
+
+        with self.assertRaises(ValueError) as cm:
+            CubeSchema(schema.shape, schema.coords, dims=('lat', 'lon', 'time'))
+        self.assertEqual("the first name in dims must be 'time'",
+                         f'{cm.exception}')
+
+        with self.assertRaises(ValueError) as cm:
+            CubeSchema(schema.shape, schema.coords, dims=('time', 'lon', 'lat'))
+        self.assertEqual("the last two names in dims must be either ('lat', 'lon') or ('y', 'x')",
+                         f'{cm.exception}')
+
+        with self.assertRaises(ValueError) as cm:
+            CubeSchema(schema.shape, schema.coords, dims=schema.dims, chunks=(90, 90))
+        self.assertEqual("chunks must have same length as shape",
+                         f'{cm.exception}')
+
+        with self.assertRaises(ValueError) as cm:
+            coords = dict(schema.coords)
+            del coords['lat']
+            CubeSchema(schema.shape, coords, dims=schema.dims, chunks=(1, 90, 90))
+        self.assertEqual("missing dimension 'lat' in coords",
+                         f'{cm.exception}')
+
+        with self.assertRaises(ValueError) as cm:
+            coords = dict(schema.coords)
+            lat = coords['lat']
+            coords['lat'] = xr.DataArray(lat.values.reshape((1, len(lat))), dims=('b', lat.dims[0]), attrs=lat.attrs)
+            CubeSchema(schema.shape, coords, dims=schema.dims, chunks=(1, 90, 90))
+        self.assertEqual("labels of 'lat' in coords must be one-dimensional",
+                         f'{cm.exception}')
+
+        with self.assertRaises(ValueError) as cm:
+            coords = dict(schema.coords)
+            lat = coords['lat']
+            coords['lat'] = xr.DataArray(lat.values[1:], dims=('lat',), attrs=lat.attrs)
+            CubeSchema(schema.shape, coords, dims=schema.dims, chunks=(1, 90, 90))
+        self.assertEqual("number of labels of 'lat' in coords does not match shape",
+                         f'{cm.exception}')
+
+    def test_new_with_cube(self):
+        cube = new_cube()
+        with self.assertRaises(ValueError) as cm:
+            CubeSchema.new(cube)
+        self.assertEqual("cube is empty",
+                         f'{cm.exception}')
+
+        cube = new_cube(variables=dict(a=1, b=2))
+        cube['c'] = xr.DataArray(np.array([1, 2, 3, 4, 5]), dims=('q',))
+        with self.assertRaises(ValueError) as cm:
+            CubeSchema.new(cube)
+        self.assertEqual("all variables must have same dimensions, but variable 'c' has dimensions ('q',)",
+                         f'{cm.exception}')
+
+        cube = new_cube(variables=dict(a=1, b=2))
+        cube = cube.chunk(dict(time=1, lat=90, lon=90))
+        cube['b'] = cube['b'].chunk(dict(time=1, lat=45, lon=90))
+        with self.assertRaises(ValueError) as cm:
+            CubeSchema.new(cube)
+        self.assertEqual("all variables must have same chunks, but variable 'b' has chunks (1, 45, 90)",
+                         f'{cm.exception}')
+
+        cube = new_cube(variables=dict(a=1, b=2))
+        cube = cube.chunk(dict(time=1, lat=(44, 43, 46, 47), lon=90))
+        with self.assertRaises(ValueError) as cm:
+            CubeSchema.new(cube)
+        self.assertEqual("dimension 'lat' of variable 'a' has chunks of different sizes: (44, 43, 46, 47)",
+                         f'{cm.exception}')
+
--- xcube/core/schema.py
+++ xcube/core/schema.py
@@ -19,7 +19,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-from typing import Tuple, Sequence, Dict, Optional
+from typing import Tuple, Sequence, Dict, Optional, Mapping
 
 import numpy as np
 import xarray as xr
@@ -38,19 +38,25 @@ class CubeSchema:
 
     def __init__(self,
                  shape: Sequence[int],
-                 coords: Dict[str, np.array],
+                 coords: Mapping[str, np.array],
                  dims: Sequence[str] = None,
                  chunks: Sequence[int] = None):
 
         if not shape:
-            raise ValueError('shape must a sequence of integer sizes')
+            raise ValueError('shape must be a sequence of integer sizes')
         if not coords:
-            raise ValueError('coords must a mapping from dimension names to label arrays')
+            raise ValueError('coords must be a mapping from dimension names to label arrays')
 
         ndim = len(shape)
-        dims = dims or ('time', 'lat', 'lon')
+        if ndim < 3:
+            raise ValueError('shape must have at least three dimensions')
+        dims = tuple(dims) or ('time', 'lat', 'lon')
         if dims and len(dims) != ndim:
             raise ValueError('dims must have same length as shape')
+        if dims[0] != 'time':
+            raise ValueError("the first name in dims must be 'time'")
+        if dims[-2:] not in (('lat', 'lon'), ('y', 'x')):
+            raise ValueError("the last two names in dims must be either ('lat', 'lon') or ('y', 'x')")
         if chunks and len(chunks) != ndim:
             raise ValueError('chunks must have same length as shape')
         for i in range(ndim):
@@ -65,7 +71,7 @@ class CubeSchema:
                 raise ValueError(f'number of labels of {dim_name!r} in coords does not match shape')
 
         self._shape = tuple(shape)
-        self._dims = tuple(dims)
+        self._dims = dims
         self._chunks = tuple(chunks) if chunks else None
         self._coords = dict(coords)
 
@@ -75,12 +81,22 @@ class CubeSchema:
         return len(self._dims)
 
     @property
-    def dims(self) -> Tuple[str]:
+    def dims(self) -> Tuple[str, ...]:
         """Tuple of dimension names."""
         return self._dims
 
     @property
-    def shape(self) -> Tuple[int]:
+    def time_dim(self) -> str:
+        """Name of time dimension."""
+        return self._dims[0]
+
+    @property
+    def spatial_dims(self) -> Tuple[str, str]:
+        """Tuple (pair) of spatial dimension names, will be either ('lat', 'lon') or ('y', 'x')."""
+        return self._dims[-2:]
+
+    @property
+    def shape(self) -> Tuple[int, ...]:
         """Tuple of dimension sizes."""
         return self._shape
 
@@ -99,9 +115,25 @@ class CubeSchema:
         """Create a cube schema from given *cube*."""
         return get_cube_schema(cube)
 
+    def _repr_html_(self):
+        """Return a HTML representation for Jupyter Notebooks."""
+        return (
+            f'<table>'
+            f'<tr><td>Shape:</td><td>{self.shape}</td></tr>'
+            f'<tr><td>Chunk sizes:</td><td>{self.chunks}</td></tr>'
+            f'<tr><td>Dimensions:</td><td>{self.dims}</td></tr>'
+            f'</table>'
+        )
+
 
 # TODO (forman): code duplication with xcube.core.verify._check_data_variables(), line 76
 def get_cube_schema(cube: xr.Dataset) -> CubeSchema:
+    """
+    Derive cube schema from given *cube*.
+
+    :param cube: The data cube.
+    :return: The cube schema.
+    """
     first_dims = None
     first_shape = None
     first_chunks = None
@@ -128,25 +160,24 @@ def get_cube_schema(cube: xr.Dataset) -> CubeSchema:
             first_coords = coords
 
         dask_chunks = var.chunks
-        if dask_chunks is None:
-            raise ValueError(f'all variables must be chunked, but variable {var_name!r} is not')
-        chunks = []
-        for i in range(var.ndim):
-            dim_name = var.dims[i]
-            dim_chunk_sizes = dask_chunks[i]
-            first_size = dim_chunk_sizes[0]
-            if any(size != first_size for size in dim_chunk_sizes[1:-1]):
-                raise ValueError(f'dimension {dim_name!r} of variable {var_name!r} has chunks of different sizes: '
-                                 f'{dim_chunk_sizes!r}')
-            chunks.append(first_size)
-        chunks = tuple(chunks)
-        if first_chunks is None:
-            first_chunks = chunks
-        elif first_chunks != chunks:
-            raise ValueError(f'all variables must have same chunks, but variable {var_name!r} '
-                             f'has chunks {chunks!r}')
+        if dask_chunks:
+            chunks = []
+            for i in range(var.ndim):
+                dim_name = var.dims[i]
+                dim_chunk_sizes = dask_chunks[i]
+                first_size = dim_chunk_sizes[0]
+                if any(size != first_size for size in dim_chunk_sizes[1:-1]):
+                    raise ValueError(f'dimension {dim_name!r} of variable {var_name!r} has chunks of different sizes: '
+                                     f'{dim_chunk_sizes!r}')
+                chunks.append(first_size)
+            chunks = tuple(chunks)
+            if first_chunks is None:
+                first_chunks = chunks
+            elif first_chunks != chunks:
+                raise ValueError(f'all variables must have same chunks, but variable {var_name!r} '
+                                 f'has chunks {chunks!r}')
 
     if first_dims is None:
         raise ValueError('cube is empty')
 
-    return CubeSchema(first_shape, first_coords, dims=first_dims, chunks=first_chunks)
+    return CubeSchema(first_shape, first_coords, dims=tuple(str(d) for d in first_dims), chunks=first_chunks)
