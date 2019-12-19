66a8965c311e1ea9350ed08e397856ecaf5e2290
AliceBalfanz <alicja.balfanz@brockmann-consult.de>
2019-12-19 14:34:37 +0100

    changing table header based on comment of forman

--- examples/notebooks/show_remote_cubes.py
+++ examples/notebooks/show_remote_cubes.py
@@ -1,5 +1,3 @@
-import os
-
 import pandas as pd
 import s3fs
 
@@ -7,7 +5,6 @@ from xcube.core.dsio import open_cube
 
 
 def show_remote_cubes(bucket, endpoint_url, region_name):
-
     s3_client_kwargs = {}
     s3_client_kwargs['endpoint_url'] = endpoint_url
     s3_client_kwargs['region_name'] = region_name
@@ -15,12 +12,11 @@ def show_remote_cubes(bucket, endpoint_url, region_name):
 
     cube_names = []
     df = pd.DataFrame(
-        columns=['cube_name', 'chunks (time, lat, lon)', 'number_of_variables', 'variables',
-                 'start_date', 'end_date', 'spatial_coverage (lon_min, lat_min, lon_max, lat_max)'])
+        columns=['cube_name', 'chunks', 'number_of_variables', 'variables',
+                 'start_date', 'end_date', 'spatial_coverage'])
 
     for filepath in sorted(obs_file_system.ls(bucket)):
         if filepath.endswith('.zarr'):
-            # ds = read_cube(f'{endpoint_url}/{filepath}', consolidated=True)
             with open_cube(f'{endpoint_url}/{filepath}') as ds:
                 var_list = list(ds.data_vars)
                 cube_names.append(filepath)
@@ -29,17 +25,23 @@ def show_remote_cubes(bucket, endpoint_url, region_name):
                 start_date = sd.strftime('%Y-%m-%d')
                 ed = pd.to_datetime(str(ds.time.values[-1]))
                 end_date = ed.strftime('%Y-%m-%d')
-                chunksize = ds[var_list[0]].data.chunksize
+                chunksize = []
+                for idx, dim in enumerate(ds[var_list[0]].dims):
+                    chunksize.append(f"{dim}: {ds[var_list[0]].data.chunksize[idx]}")
                 try:
-                    spat_cov = (ds.attrs['geospatial_lon_min'], ds.attrs['geospatial_lat_min'],
-                                ds.attrs['geospatial_lon_max'], ds.attrs['geospatial_lat_max'])
+                    spat_cov = ([
+                        f"lon_min: {ds.attrs['geospatial_lon_min']}",
+                        f"lat_min: {ds.attrs['geospatial_lat_min']}",
+                        f"lon_max: {ds.attrs['geospatial_lon_max']}",
+                        f"lat_max: {ds.attrs['geospatial_lat_max']}"])
                 except KeyError:
                     spat_cov = None
                 df = df.append({'cube_name': filename,
-                                'chunks (time, lat, lon)': chunksize,
+                                'chunks': ', '.join(chunksize),
                                 'number_of_variables': len(var_list),
-                                'variables': (str(var_list)).replace('[', '').replace(']', '').replace("'", ""),
+                                'variables': ', '.join(var_list),
                                 'start_date': start_date,
                                 'end_date': end_date,
-                                'spatial_coverage (lon_min, lat_min, lon_max, lat_max)': str(spat_cov)}, ignore_index=True)
+                                'spatial_coverage': ', '.join(spat_cov)},
+                               ignore_index=True)
     return df
