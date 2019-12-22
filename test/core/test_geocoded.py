ba201272705f6b4dbde75cc0200d57d2bae8db15
Norman Fomferra <norman.fomferra@gmail.com>
2019-12-22 20:09:40 +0100

    more testing

--- test/core/test_geocoded.py
+++ test/core/test_geocoded.py
@@ -1,77 +1,207 @@
-import time
 import unittest
 
 import numpy as np
 import xarray as xr
 
-from xcube.core.geocoded import compute_output_geom, reproject
+from xcube.core.geocoded import compute_output_geom, reproject_dataset, ImageGeom
 
 TEST_INPUT = '../xcube-gen-bc/test/inputdata/O_L2_0001_SNS_2017104102450_v1.0.nc'
 
+nan = np.nan
+
 
 class ReprojectTest(unittest.TestCase):
+
+    def new_source_dataset(self):
+        lon = np.array([[1.0, 6.0],
+                        [0.0, 2.0]])
+        lat = np.array([[56.0, 53.0],
+                        [52.0, 50.0]])
+        rad = np.array([[1.0, 2.0],
+                        [3.0, 4.0]])
+        return xr.Dataset(dict(lon=xr.DataArray(lon, dims=('y', 'x')),
+                               lat=xr.DataArray(lat, dims=('y', 'x')),
+                               rad=xr.DataArray(rad, dims=('y', 'x'))))
+
+    def new_source_dataset_antimeridian(self):
+        lon = np.array([[+179.0, -176.0],
+                        [+178.0, +180.0]])
+        lat = np.array([[56.0, 53.0],
+                        [52.0, 50.0]])
+        rad = np.array([[1.0, 2.0],
+                        [3.0, 4.0]])
+        return xr.Dataset(dict(lon=xr.DataArray(lon, dims=('y', 'x')),
+                               lat=xr.DataArray(lat, dims=('y', 'x')),
+                               rad=xr.DataArray(rad, dims=('y', 'x'))))
+
+    def test_reproject_2x2_to_default(self):
+        src_ds = self.new_source_dataset()
+
+        dst_ds = reproject_dataset(src_ds)
+        lon, lat, rad = self._assert_shape_and_dim(dst_ds, 4, 4)
+        np.testing.assert_almost_equal(lon.values,
+                                       np.array([0.0, 2.0, 4.0, 6.0],
+                                                dtype=lon.dtype))
+        np.testing.assert_almost_equal(lat.values,
+                                       np.array([50.0, 52.0, 54.0, 56.0],
+                                                dtype=lat.dtype))
+        np.testing.assert_almost_equal(rad.values,
+                                       np.array([
+                                           [nan, 4.0, nan, nan],
+                                           [3.0, 3.0, 2.0, nan],
+                                           [nan, 1.0, 2.0, nan],
+                                           [nan, nan, nan, nan]
+                                       ], dtype=rad.dtype))
+
+    def test_reproject_2x2_to_7x7(self):
+        src_ds = self.new_source_dataset()
+
+        output_geom = ImageGeom(width=7, height=7, x_min=0.0, y_min=50.0, res=1.0)
+
+        dst_ds = reproject_dataset(src_ds, output_geom=output_geom)
+        lon, lat, rad = self._assert_shape_and_dim(dst_ds, 7, 7)
+        np.testing.assert_almost_equal(lon.values,
+                                       np.array([0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0],
+                                                dtype=lon.dtype))
+        np.testing.assert_almost_equal(lat.values,
+                                       np.array([50.0, 51.0, 52.0, 53.0, 54.0, 55.0, 56.0],
+                                                dtype=lat.dtype))
+        np.testing.assert_almost_equal(rad.values,
+                                       np.array([
+                                           [nan, nan, 4.0, nan, nan, nan, nan],
+                                           [nan, 3.0, 4.0, 4.0, nan, nan, nan],
+                                           [3.0, 3.0, 3.0, 4.0, 2.0, nan, nan],
+                                           [nan, 3.0, 3.0, 1.0, 2.0, 2.0, 2.0],
+                                           [nan, 1.0, 1.0, 1.0, 2.0, nan, nan],
+                                           [nan, 1.0, 1.0, nan, nan, nan, nan],
+                                           [nan, 1.0, nan, nan, nan, nan, nan],
+                                       ], dtype=rad.dtype))
+
+    def test_reproject_2x2_to_13x13(self):
+        src_ds = self.new_source_dataset()
+
+        output_geom = ImageGeom(width=13, height=13, x_min=0.0, y_min=50.0, res=0.5)
+
+        dst_ds = reproject_dataset(src_ds, output_geom=output_geom)
+        lon, lat, rad = self._assert_shape_and_dim(dst_ds, 13, 13)
+        np.testing.assert_almost_equal(lon.values,
+                                       np.array([0.0, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0, 5.5, 6.0],
+                                                dtype=lon.dtype))
+        np.testing.assert_almost_equal(lat.values,
+                                       np.array([50.0, 50.5, 51.0, 51.5, 52.0, 52.5, 53.0, 53.5, 54.0, 54.5, 55.0, 55.5,
+                                                 56.0],
+                                                dtype=lat.dtype))
+        np.testing.assert_almost_equal(rad.values,
+                                       np.array([
+                                           [nan, nan, nan, nan, 4.0, nan, nan, nan, nan, nan, nan, nan, nan],
+                                           [nan, nan, nan, 4.0, 4.0, 4.0, nan, nan, nan, nan, nan, nan, nan],
+                                           [nan, nan, 3.0, 4.0, 4.0, 4.0, 4.0, nan, nan, nan, nan, nan, nan],
+                                           [nan, 3.0, 3.0, 3.0, 4.0, 4.0, 4.0, 4.0, 2.0, nan, nan, nan, nan],
+                                           [3.0, 3.0, 3.0, 3.0, 3.0, 4.0, 4.0, 2.0, 2.0, 2.0, nan, nan, nan],
+                                           [nan, 3.0, 3.0, 3.0, 3.0, 3.0, 4.0, 2.0, 2.0, 2.0, 2.0, nan, nan],
+                                           [nan, 3.0, 3.0, 3.0, 3.0, 1.0, 1.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.],
+                                           [nan, 3.0, 3.0, 1.0, 1.0, 1.0, 1.0, 2.0, 2.0, 2.0, 2.0, nan, nan],
+                                           [nan, 3.0, 1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 2.0, nan, nan, nan, nan],
+                                           [nan, nan, 1.0, 1.0, 1.0, 1.0, 1.0, 2.0, nan, nan, nan, nan, nan],
+                                           [nan, nan, 1.0, 1.0, 1.0, 1.0, nan, nan, nan, nan, nan, nan, nan],
+                                           [nan, nan, 1.0, 1.0, nan, nan, nan, nan, nan, nan, nan, nan, nan],
+                                           [nan, nan, 1.0, nan, nan, nan, nan, nan, nan, nan, nan, nan, nan],
+                                       ], dtype=rad.dtype))
+
+    def test_reproject_2x2_to_13x13_antimeridian(self):
+        src_ds = self.new_source_dataset_antimeridian()
+
+        output_geom = ImageGeom(width=13, height=13, x_min=+178.0, y_min=+50.0, res=0.5)
+
+        dst_ds = reproject_dataset(src_ds, output_geom=output_geom)
+        lon, lat, rad = self._assert_shape_and_dim(dst_ds, 13, 13)
+        np.testing.assert_almost_equal(lon.values,
+                                       np.array([+178.0, +178.5, +179.0, +179.5, +180.0,
+                                                 -179.5, -179.0, -178.5, -178.0, -177.5, -177.0, -176.5, -176.0],
+                                                dtype=lon.dtype))
+        np.testing.assert_almost_equal(lat.values,
+                                       np.array([50.0, 50.5, 51.0, 51.5, 52.0,
+                                                 52.5, 53.0, 53.5, 54.0, 54.5, 55.0, 55.5, 56.0],
+                                                dtype=lat.dtype))
+        np.testing.assert_almost_equal(rad.values,
+                                       np.array([
+                                           [nan, nan, nan, nan, 4.0, nan, nan, nan, nan, nan, nan, nan, nan],
+                                           [nan, nan, nan, 4.0, 4.0, 4.0, nan, nan, nan, nan, nan, nan, nan],
+                                           [nan, nan, 3.0, 4.0, 4.0, 4.0, 4.0, nan, nan, nan, nan, nan, nan],
+                                           [nan, 3.0, 3.0, 3.0, 4.0, 4.0, 4.0, 4.0, 2.0, nan, nan, nan, nan],
+                                           [3.0, 3.0, 3.0, 3.0, 3.0, 4.0, 4.0, 2.0, 2.0, 2.0, nan, nan, nan],
+                                           [nan, 3.0, 3.0, 3.0, 3.0, 3.0, 4.0, 2.0, 2.0, 2.0, 2.0, nan, nan],
+                                           [nan, 3.0, 3.0, 3.0, 3.0, 1.0, 1.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.],
+                                           [nan, 3.0, 3.0, 1.0, 1.0, 1.0, 1.0, 2.0, 2.0, 2.0, 2.0, nan, nan],
+                                           [nan, 3.0, 1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 2.0, nan, nan, nan, nan],
+                                           [nan, nan, 1.0, 1.0, 1.0, 1.0, 1.0, 2.0, nan, nan, nan, nan, nan],
+                                           [nan, nan, 1.0, 1.0, 1.0, 1.0, nan, nan, nan, nan, nan, nan, nan],
+                                           [nan, nan, 1.0, 1.0, nan, nan, nan, nan, nan, nan, nan, nan, nan],
+                                           [nan, nan, 1.0, nan, nan, nan, nan, nan, nan, nan, nan, nan, nan],
+                                       ], dtype=rad.dtype))
+
+    def _assert_shape_and_dim(self, dst_ds, w, h):
+        self.assertIn('lon', dst_ds)
+        lon = dst_ds['lon']
+        self.assertEqual((w,), lon.shape)
+        self.assertEqual(('lon',), lon.dims)
+
+        self.assertIn('lat', dst_ds)
+        lat = dst_ds['lat']
+        self.assertEqual((h,), lat.shape)
+        self.assertEqual(('lat',), lat.dims)
+
+        self.assertIn('rad', dst_ds)
+        rad = dst_ds['rad']
+        self.assertEqual((h, w), rad.shape)
+        self.assertEqual(('lat', 'lon'), rad.dims)
+
+        return lon, lat, rad
+
     def test_compute_output_geom(self):
-        src_ds = xr.open_dataset(TEST_INPUT)
+        src_ds = self.new_source_dataset()
 
-        width, height, lon_min, lat_min, res = compute_output_geom(src_ds)
-        self.assertEqual(834, width)
-        self.assertEqual(535, height)
-        self.assertAlmostEqual(1.152138, lon_min, delta=1e-5)
-        self.assertAlmostEqual(52.32967, lat_min, delta=1e-5)
-        self.assertAlmostEqual(0.0005832, res, delta=1e-6)
+        self._assert_image_geom(ImageGeom(4, 4, 0.0, 50.0, 2.0),
+                                compute_output_geom(src_ds))
 
-        width, height, lon_min, lat_min, res = compute_output_geom(src_ds, denom_x=4, denom_y=2)
-        self.assertEqual(836, width)
-        self.assertEqual(536, height)
-        self.assertAlmostEqual(1.152138, lon_min, delta=1e-5)
-        self.assertAlmostEqual(52.32967, lat_min, delta=1e-5)
-        self.assertAlmostEqual(0.0005832, res, delta=1e-6)
+        self._assert_image_geom(ImageGeom(7, 7, 0.0, 50.0, 1.0),
+                                compute_output_geom(src_ds,
+                                                    oversampling=2.0))
 
+        self._assert_image_geom(ImageGeom(8, 8, 0.0, 50.0, 1.0),
+                                compute_output_geom(src_ds,
+                                                    denom_x=4,
+                                                    denom_y=4,
+                                                    oversampling=2.0))
 
-    def test_reproject(self):
-        src_ds = xr.open_dataset(TEST_INPUT)
-        src_var = src_ds.rtoa_8
+    def test_compute_output_geom_antimeridian(self):
+        src_ds = self.new_source_dataset_antimeridian()
 
-        dst_width, dst_height, lon_min, lat_max, dst_res = compute_output_geom(src_ds)
+        self._assert_image_geom(ImageGeom(4, 4, 178.0, 50.0, 2.0),
+                                compute_output_geom(src_ds))
 
-        dst_var_values = np.full((dst_height, dst_width), np.nan, dtype=src_var.dtype)
-        t1 = time.perf_counter()
-        reproject(src_var.values,
-                  src_ds.lon.values,
-                  src_ds.lat.values,
-                  dst_var_values,
-                  lon_min,
-                  lat_max,
-                  dst_res)
-        t2 = time.perf_counter()
-        print('time:', t2 - t1)
+        self._assert_image_geom(ImageGeom(7, 7, 178.0, 50.0, 1.0),
+                                compute_output_geom(src_ds,
+                                                    oversampling=2.0))
 
-        self.assertEqual(301623, np.sum(np.where(np.isnan(dst_var_values), 0, 1)))
+        self._assert_image_geom(ImageGeom(8, 8, 178.0, 50.0, 1.0),
+                                compute_output_geom(src_ds,
+                                                    denom_x=4,
+                                                    denom_y=4,
+                                                    oversampling=2.0))
 
-        self.assertAlmostEqual(float(src_var.min()),
-                               float(np.nanmin(dst_var_values)),
-                               delta=1e-10)
+    def _assert_image_geom(self,
+                           expected: ImageGeom,
+                           actual: ImageGeom):
+        self.assertEqual(expected.width, actual.width)
+        self.assertEqual(expected.height, actual.height)
+        self.assertAlmostEqual(actual.x_min, actual.x_min, delta=1e-5)
+        self.assertAlmostEqual(actual.y_min, actual.y_min, delta=1e-5)
+        self.assertAlmostEqual(actual.res, actual.res, delta=1e-6)
 
-        self.assertAlmostEqual(float(src_var.max()),
-                               float(np.nanmax(dst_var_values)),
-                               delta=1e-10)
+    def test_image_geom_is_crossing_antimeridian(self):
+        output_geom = ImageGeom(width=13, height=13, x_min=0.0, y_min=+50.0, res=0.5)
+        self.assertFalse(output_geom.is_crossing_antimeridian)
 
-        self.assertAlmostEqual(float(src_var.mean()),
-                               float(np.nanmean(dst_var_values)),
-                               delta=1e-2)
-
-        lon_start = lon_min + 0.5 * dst_res
-        lat_start = lat_max + 0.5 * dst_res
-        dst_var = xr.DataArray(dst_var_values,
-                               dims=('lat', 'lon'),
-                               coords=dict(lon=np.linspace(lon_start,
-                                                           lon_start + (dst_width - 1) * dst_res,
-                                                           num=dst_width,
-                                                           dtype=np.float64),
-                                           lat=np.linspace(lat_start,
-                                                           lat_start + (dst_height - 1) * dst_res,
-                                                           num=dst_height,
-                                                           dtype=np.float64)),
-                               attrs=src_var.attrs)
-        dst_ds = xr.Dataset({src_var.name: dst_var})
-        dst_ds.to_netcdf('reprojected.nc')
+        output_geom = ImageGeom(width=13, height=13, x_min=178.0, y_min=+50.0, res=0.5)
+        self.assertTrue(output_geom.is_crossing_antimeridian)
--- xcube/core/geocoded.py
+++ xcube/core/geocoded.py
@@ -1,8 +1,16 @@
+import collections
 import math
+from typing import Sequence, Tuple
 
 import numba as nb
 import numpy as np
 import xarray as xr
+
+
+class ImageGeom(collections.namedtuple('ImageGeometry', ['width', 'height', 'x_min', 'y_min', 'res'])):
+    @property
+    def is_crossing_antimeridian(self):
+        return self.x_min + self.width * self.res > 180.0
 
 
 @nb.jit(nopython=True, inline='always')
@@ -27,7 +35,8 @@ def reproject(src_values: np.ndarray,
               dst_values: np.ndarray,
               dst_x0: float,
               dst_y0: float,
-              dst_res: float):
+              dst_res: float,
+              delta: float = 1e-3):
     src_width = src_values.shape[-1]
     src_height = src_values.shape[-2]
 
@@ -36,6 +45,9 @@ def reproject(src_values: np.ndarray,
 
     dst_px = np.zeros(4, dtype=src_x.dtype)
     dst_py = np.zeros(4, dtype=src_y.dtype)
+
+    u_min = v_min = -delta
+    uv_max = 1.0 + 2 * delta
 
     for src_j0 in range(src_height - 1):
         for src_i0 in range(src_width - 1):
@@ -52,14 +64,8 @@ def reproject(src_values: np.ndarray,
             dst_py[2] = dst_py2 = src_y[src_j1, src_i0]
             dst_py[3] = dst_py3 = src_y[src_j1, src_i1]
 
-            dst_pi_f = (dst_px - dst_x0) / dst_res
-            dst_pj_f = (dst_py - dst_y0) / dst_res
-
-            dst_pi = np.floor(dst_pi_f).astype(np.int64)
-            dst_pj = np.floor(dst_pj_f).astype(np.int64)
-
-            # dst_pi_f -= dst_pi
-            # dst_pj_f -= dst_pj
+            dst_pi = np.floor((dst_px - dst_x0) / dst_res).astype(np.int64)
+            dst_pj = np.floor((dst_py - dst_y0) / dst_res).astype(np.int64)
 
             dst_i_min = np.min(dst_pi)
             dst_i_max = np.max(dst_pi)
@@ -98,37 +104,164 @@ def reproject(src_values: np.ndarray,
                     if det_a != 0.0:
                         u = _fu(dst_x, dst_y, dst_px0, dst_py0, dst_px2, dst_py2) / det_a
                         v = _fv(dst_x, dst_y, dst_px0, dst_py0, dst_px1, dst_py1) / det_a
-                        if u >= 0.0 and v >= 0.0 and u + v <= 1.0:
+                        if u >= u_min and v >= v_min and u + v <= uv_max:
                             src_i = src_i0 if u < 0.5 else src_i1
                             src_j = src_j0 if v < 0.5 else src_j1
                     if src_i == -1 and det_b != 0.0:
                         u = _fu(dst_x, dst_y, dst_px3, dst_py3, dst_px1, dst_py1) / det_b
                         v = _fv(dst_x, dst_y, dst_px3, dst_py3, dst_px2, dst_py2) / det_b
-                        if u >= 0.0 and v >= 0.0 and u + v <= 1.0:
+                        if u >= u_min and v >= v_min and u + v <= uv_max:
                             src_i = src_i1 if u < 0.5 else src_i0
                             src_j = src_j1 if v < 0.5 else src_j0
                     if src_i != -1:
                         dst_values[dst_j, dst_i] = src_values[src_j, src_i]
 
 
-def compute_output_geom(src_ds: xr.Dataset, denom_x: int = 1, denom_y: int = 1, delta: float = 1e-10):
-    lon_min = float(src_ds.lon.min())
-    lon_max = float(src_ds.lon.max())
-    lat_min = float(src_ds.lat.min())
-    lat_max = float(src_ds.lat.max())
-    lon_x_diff = abs(src_ds.lon.diff(dim='x'))
-    lon_y_diff = abs(src_ds.lon.diff(dim='y'))
-    lat_x_diff = abs(src_ds.lat.diff(dim='x'))
-    lat_y_diff = abs(src_ds.lat.diff(dim='y'))
-    lon_x_res = lon_x_diff.where(lon_x_diff > delta).min()
-    lon_y_res = lon_y_diff.where(lon_y_diff > delta).min()
-    lat_x_res = lat_x_diff.where(lat_x_diff > delta).min()
-    lat_y_res = lat_y_diff.where(lat_y_diff > delta).min()
-    lon_res = float(min(lon_x_res, lon_y_res))
-    lat_res = float(min(lat_x_res, lat_y_res))
-    res = min(lon_res, lat_res)
-    width = 1 + math.floor((lon_max - lon_min) / res)
-    height = 1 + math.floor((lat_max - lat_min) / res)
-    return denom_x * ((width + denom_x - 1) // denom_x), \
-           denom_y * ((height + denom_y - 1) // denom_y), \
-           lon_min, lat_min, res
+def compute_output_geom(src_ds: xr.Dataset,
+                        x_name: str = 'lon',
+                        y_name: str = 'lat',
+                        oversampling: float = 1.0,
+                        denom_x: int = 1,
+                        denom_y: int = 1,
+                        delta: float = 1e-10) -> ImageGeom:
+    src_x, src_y, normalized_lon = _get_2d_coords(src_ds, x_name=x_name, y_name=y_name)
+    src_x_x_diff = src_x.diff(dim='x')
+    src_x_y_diff = src_x.diff(dim='y')
+    src_y_x_diff = src_y.diff(dim='x')
+    src_y_y_diff = src_y.diff(dim='y')
+    src_x_x_diff_sq = np.square(src_x_x_diff)
+    src_x_y_diff_sq = np.square(src_x_y_diff)
+    src_y_x_diff_sq = np.square(src_y_x_diff)
+    src_y_y_diff_sq = np.square(src_y_y_diff)
+    src_x_diff = np.sqrt(src_x_x_diff_sq + src_y_x_diff_sq)
+    src_y_diff = np.sqrt(src_x_y_diff_sq + src_y_y_diff_sq)
+    src_x_res = float(src_x_diff.where(src_x_diff > delta).min())
+    src_y_res = float(src_y_diff.where(src_y_diff > delta).min())
+    src_res = min(src_x_res, src_y_res) / (math.sqrt(2.0) * oversampling)
+    src_x_min = float(src_x.min())
+    src_x_max = float(src_x.max())
+    src_y_min = float(src_y.min())
+    src_y_max = float(src_y.max())
+    dst_width = 1 + math.floor((src_x_max - src_x_min) / src_res)
+    dst_height = 1 + math.floor((src_y_max - src_y_min) / src_res)
+    return ImageGeom(width=denom_x * ((dst_width + denom_x - 1) // denom_x),
+                     height=denom_y * ((dst_height + denom_y - 1) // denom_y),
+                     x_min=src_x_min,
+                     y_min=src_y_min,
+                     res=src_res)
+
+
+def _get_2d_coord(src_ds: xr.Dataset, name: str) -> xr.DataArray:
+    if name not in src_ds:
+        raise ValueError(f'missing 2D coordinate variable {name!r}')
+    var = src_ds[name]
+    if var.ndim != 2:
+        raise ValueError(f'coordinate variable {name!r} must have two dimensions')
+    return var
+
+
+def _get_2d_coords(src_ds: xr.Dataset,
+                   x_name: str = 'lon',
+                   y_name: str = 'lat') -> Tuple[xr.DataArray, xr.DataArray, bool]:
+    src_x = _get_2d_coord(src_ds, x_name)
+    src_y = _get_2d_coord(src_ds, y_name)
+
+    if src_x.shape != src_y.shape or src_x.dims != src_y.dims:
+        raise ValueError(f"coordinate variables {x_name!r} and {y_name!r} must have same shape and dimensions")
+
+    src_width, src_height = src_x.shape
+    if src_width < 2 or src_height < 2:
+        raise ValueError(f"size in each dimension of {x_name!r} and {y_name!r} must be greater two")
+
+    normalized_lon = False
+    if x_name in ('lon', 'long', 'longitude'):
+        src_x, normalized_lon = _maybe_normalise_lon(src_x)
+
+    return src_x, src_y, normalized_lon
+
+
+def _is_2d_var(var: xr.DataArray, coord_var: xr.DataArray) -> bool:
+    return var.ndim >= 2 and var.shape[-2:] == coord_var.shape and var.dims[-2:] == coord_var.dims
+
+
+def _is_crossing_antimeridian(lon_var: xr.DataArray):
+    dim_x = lon_var.dims[-1]
+    dim_y = lon_var.dims[-2]
+    return abs(lon_var.diff(dim=dim_x)).max() > 180.0 or \
+           abs(lon_var.diff(dim=dim_y)).max() > 180.0
+
+
+def _maybe_normalise_lon(lon_var: xr.DataArray):
+    if _is_crossing_antimeridian(lon_var):
+        lon_var = _normalize_lon(lon_var)
+        if _is_crossing_antimeridian(lon_var):
+            raise ValueError('cannot account for longitudial anti-meridian crossing')
+        return lon_var, True
+    return lon_var, False
+
+
+def _normalize_lon(lon_var: xr.DataArray):
+    return lon_var.where(lon_var >= 0.0, lon_var + 360.0)
+
+
+def _denormalize_lon(lon_var: xr.DataArray):
+    return lon_var.where(lon_var <= 180.0, lon_var - 360.0)
+
+
+def reproject_dataset(src_ds: xr.Dataset,
+                      var_names: Sequence[str] = None,
+                      x_name: str = 'lon',
+                      y_name: str = 'lat',
+                      output_geom: ImageGeom = None,
+                      delta: float = 1e-3):
+    src_x, src_y, normalized_lon = _get_2d_coords(src_ds, x_name=x_name, y_name=y_name)
+
+    if var_names is None:
+        var_names = [var_name for var_name, var in src_ds.data_vars.items()
+                     if var_name not in (x_name, y_name) and _is_2d_var(var, src_x)]
+    elif isinstance(var_names, str):
+        var_names = (var_names,)
+    elif len(var_names) == 0:
+        raise ValueError(f'empty var_names')
+
+    src_vars = []
+    for var_name in var_names:
+        src_var = src_ds[var_name]
+        if not _is_2d_var(src_var, src_x):
+            raise ValueError(
+                f"cannot reproject variable {var_name!r} as its shape or dimensions "
+                f"do not match those of {x_name!r} and {y_name!r}")
+        src_vars.append(src_var)
+
+    if output_geom is None:
+        output_geom = compute_output_geom(src_ds)
+    dst_width, dst_height, dst_x_min, dst_y_min, dst_res = output_geom
+
+    dims = (y_name, x_name)
+    # TODO: add bnds vars too
+    x_var = xr.DataArray(np.linspace(dst_x_min, dst_x_min + (dst_width - 1) * dst_res,
+                                     num=dst_width, dtype=np.float64), dims=x_name)
+    y_var = xr.DataArray(np.linspace(dst_y_min, dst_y_min + (dst_height - 1) * dst_res,
+                                     num=dst_height, dtype=np.float64), dims=y_name)
+    coords = {
+        x_name: _denormalize_lon(x_var) if normalized_lon else x_var,
+        y_name: y_var
+    }
+
+    src_x_values = src_x.values
+    src_y_values = src_y.values
+
+    dst_vars = dict()
+    for src_var in src_vars:
+        dst_var_values = np.full((dst_height, dst_width), np.nan, dtype=src_var.dtype)
+        reproject(src_var.values,
+                  src_x_values,
+                  src_y_values,
+                  dst_var_values,
+                  dst_x_min,
+                  dst_y_min,
+                  dst_res,
+                  delta=delta)
+        dst_vars[src_var.name] = xr.DataArray(dst_var_values, dims=dims, coords=coords, attrs=src_var.attrs)
+
+    return xr.Dataset(dst_vars, attrs=src_ds.attrs)
