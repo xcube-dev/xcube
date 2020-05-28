2f61fb11f02a9a2b04db098139d599d6142ecaf2
Pontus Lurcock <pontus.lurcock@brockmann-consult.de>
2020-05-28 12:21:09 +0200

    Add failing unit test for fix of Issue #278

---
+++ test/cli/test_rectify.py
@@ -0,0 +1,37 @@
+import os
+from typing import List
+import xarray as xr
+from test.cli.helpers import CliDataTest, TEST_ZARR_DIR
+from xcube.core.verify import assert_cube
+
+
+class RectifyTest(CliDataTest):
+
+    def outputs(self) -> List[str]:
+        return ['out.zarr']
+
+    def test_rectify_without_vars(self):
+        """Test that rectify selects all variables when --var is not given."""
+
+        # For now, specify the image geometry explicitly with --size, --point,
+        # and --res to avoid triggering an "invalid y_min" ValueError when
+        # ImageGeom tries to determine it automatically. Once Issue #303 has
+        # been fixed, these options can be omitted.
+
+        result = self.invoke_cli(['rectify',
+                                  '--size', '508,253',
+                                  '--point', '-179.5,-89.5',
+                                  '--res', '0.7071067811865475',
+                                  TEST_ZARR_DIR])
+        self.assertEqual(0, result.exit_code)
+        self.assertEqual('Opening dataset from \'test.zarr\'...\n'
+                         'Rectifying...\n'
+                         'Writing rectified dataset to \'out.zarr\'...\n'
+                         'Done.\n',
+                         result.stdout)
+        self.assertTrue(os.path.isdir('out.zarr'))
+        ds = xr.open_zarr('out.zarr')
+        assert_cube(ds)
+        self.assertIn('precipitation', ds)
+        self.assertIn('temperature', ds)
+        self.assertIn('soil_moisture', ds)
