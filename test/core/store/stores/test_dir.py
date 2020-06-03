8d9d3aa179ae12728a63e0d72ca05b889b975737
Norman Fomferra <norman.fomferra@gmail.com>
2020-06-03 16:10:47 +0200

    fixed test

--- test/core/store/stores/test_dir.py
+++ test/core/store/stores/test_dir.py
@@ -1,14 +1,14 @@
 import os.path
 import unittest
 
-from xcube.core.store.stores.mem import DirectoryCubeStore
+from xcube.core.store.stores.dir import DirectoryCubeStore
 
 
 class DirectoryCubeStoreTest(unittest.TestCase):
 
     def test_iter_cubes(self):
         cube_store = DirectoryCubeStore(
-            base_dir=os.path.join(os.path.dirname(__file__), '..', '..', '..', 'examples', 'serve', 'demo'),
+            base_dir=os.path.join(os.path.dirname(__file__), '..', '..', '..', '..', 'examples', 'serve', 'demo'),
             read_only=True)
         self.assertEqual({'cube-1-250-250', 'cube-5-100-200'},
                          set(cube_des.id for cube_des in cube_store.iter_cubes()))
