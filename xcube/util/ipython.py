948af67247261564c25cf0014c132b5a044d9034
Norman Fomferra <norman.fomferra@gmail.com>
2020-06-10 12:50:26 +0200

    new module for IPy NB support

---
+++ xcube/util/ipython.py
@@ -0,0 +1,20 @@
+from typing import Type
+
+
+def register_json_formatter(cls: Type, to_dict_method_name: str = 'to_dict'):
+    if not hasattr(cls, to_dict_method_name) or not callable(getattr(cls, to_dict_method_name)):
+        raise ValueError(f'{cls} must define {to_dict_method_name}() method')
+
+    try:
+        import IPython
+        import IPython.display
+
+        if IPython.get_ipython() is not None:
+            def obj_to_dict(obj):
+                return getattr(obj, to_dict_method_name)()
+
+            ipy_formatter = IPython.get_ipython().display_formatter.formatters['application/json']
+            ipy_formatter.for_type(cls, obj_to_dict)
+
+    except ImportError:
+        pass
