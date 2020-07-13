66d9990feab0cd790c2eb77d652363e8015c0ff1
Pontus Lurcock <pontus.lurcock@brockmann-consult.de>
2020-07-13 16:54:55 +0200

    Make test_dump_ds compatible with xarray 0.16.0
    The test was failing because of a change in xarray behaviour in
    v0.16.0. This commit makes the test compatible with both xarray
    behaviours.

--- test/cli/test_dump.py
+++ test/cli/test_dump.py
@@ -1,18 +1,25 @@
 from test.cli.helpers import CliDataTest, TEST_NC_FILE
+import xarray as xr
 
 
 class DumpTest(CliDataTest):
 
     def test_dump_ds(self):
         self.maxDiff = None
-        result = self.invoke_cli(["dump", TEST_NC_FILE])
-        self.assertEqual((
+        with xr.set_options(display_width=80):
+            result = self.invoke_cli(["dump", TEST_NC_FILE])
+
+        # xarray v0.16.0 changed line truncation behaviour (see
+        # https://github.com/pydata/xarray/issues/3759 ), so this test checks
+        # the output against two expected values to cover both pre- and
+        # post-change behaviour.
+        output_template = (
             "<xarray.Dataset>\n"
             "Dimensions:        (bnds: 2, lat: 180, lon: 360, time: 5)\n"
             "Coordinates:\n"
             "  * lon            (lon) float64 -179.5 -178.5 -177.5 ... 177.5 178.5 179.5\n"
             "  * lat            (lat) float64 -89.5 -88.5 -87.5 -86.5 ... 86.5 87.5 88.5 89.5\n"
-            "  * time           (time) datetime64[ns] 2010-01-01T12:00:00 ... 2010-01-05T12:00:00\n"
+            "  * time           (time) datetime64[ns] 2010-01-01T12:00:00 ... 2010-01-05T1%s\n"
             "    lon_bnds       (lon, bnds) float64 ...\n"
             "    lat_bnds       (lat, bnds) float64 ...\n"
             "    time_bnds      (time, bnds) datetime64[ns] ...\n"
@@ -32,5 +39,12 @@ class DumpTest(CliDataTest):
             "    geospatial_lat_min:    -90.0\n"
             "    geospatial_lat_max:    90.0\n"
             "    geospatial_lat_units:  degrees_north\n"
-        ), result.output)
+        )
+
+        possible_outputs = [
+            output_template % '2:00:00',  # for xarray v0.15.1 and below
+            output_template % '...'  # for xarray v0.16.0 and above
+        ]
+
+        self.assertIn(result.output, possible_outputs)
         self.assertEqual(0, result.exit_code)
