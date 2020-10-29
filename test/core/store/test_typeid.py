8307cdc245b90f6f7feb3a5e01cdc67888bc5fe8
TonioF <tonio.fincke@brockmann-consult.de>
2020-10-29 15:21:13 +0100

    use proper names in testing

--- test/core/store/test_typeid.py
+++ test/core/store/test_typeid.py
@@ -16,75 +16,75 @@ from xcube.core.mldataset import BaseMultiLevelDataset
 class TypeIdTest(unittest.TestCase):
 
     def test_name(self):
-        type_id = TypeId(name='tfsr')
-        self.assertEqual('tfsr', type_id.name)
+        type_id = TypeId(name='dataset')
+        self.assertEqual('dataset', type_id.name)
         self.assertEqual(set(), type_id.flags)
 
     def test_name_and_flags(self):
-        type_id = TypeId(name='tfsr', flags={'fzht', 'gdfhg'})
-        self.assertEqual('tfsr', type_id.name)
-        self.assertEqual({'fzht', 'gdfhg'}, type_id.flags)
+        type_id = TypeId(name='dataset', flags={'cube', 'multilevel'})
+        self.assertEqual('dataset', type_id.name)
+        self.assertEqual({'cube', 'multilevel'}, type_id.flags)
 
     def test_string(self):
-        type_id = TypeId(name='tfsr', flags={'fzht', 'gdfhg'})
-        self.assertEqual(str(type_id), 'tfsr[fzht,gdfhg]')
+        type_id = TypeId(name='dataset', flags={'cube', 'multilevel'})
+        self.assertEqual(str(type_id), 'dataset[cube,multilevel]')
 
     def test_equality(self):
-        type_id = TypeId(name='tfsr', flags={'fzht'})
-        self.assertFalse(type_id == TypeId('dtftg'))
-        self.assertFalse(type_id == TypeId('dtftg', flags={'fzht'}))
-        self.assertFalse(type_id == TypeId('tfsr'))
-        self.assertFalse(type_id == TypeId('tfsr', flags={'dsf'}))
-        self.assertFalse(type_id == TypeId('tfsr', flags={'dsf', 'fzht'}))
-        self.assertTrue(type_id == TypeId('tfsr', flags={'fzht'}))
+        type_id = TypeId(name='dataset', flags={'cube'})
+        self.assertFalse(type_id == TypeId('geodataframe'))
+        self.assertFalse(type_id == TypeId('geodataframe', flags={'cube'}))
+        self.assertFalse(type_id == TypeId('dataset'))
+        self.assertFalse(type_id == TypeId('dataset', flags={'multilevel'}))
+        self.assertFalse(type_id == TypeId('dataset', flags={'multilevel', 'cube'}))
+        self.assertTrue(type_id == TypeId('dataset', flags={'cube'}))
 
     def test_equality_flag_order_is_irrelevant(self):
-        type_id_1 = TypeId(name='tfsr', flags={'fzht', 'duxdrh5g'})
-        type_id_2 = TypeId(name='tfsr', flags={'duxdrh5g', 'fzht'})
+        type_id_1 = TypeId(name='dataset', flags={'cube', 'multilevel'})
+        type_id_2 = TypeId(name='dataset', flags={'multilevel', 'cube'})
         self.assertTrue(type_id_1  == type_id_2)
 
     def test_equality_any(self):
-        type_id = TypeId(name='*', flags={'fzht'})
-        self.assertFalse(type_id == TypeId('dtftg'))
-        self.assertFalse(type_id == TypeId('dtftg', flags={'fzht'}))
-        self.assertFalse(type_id == TypeId('tfsr'))
-        self.assertFalse(type_id == TypeId('tfsr', flags={'dsf'}))
-        self.assertFalse(type_id == TypeId('tfsr', flags={'dsf', 'fzht'}))
-        self.assertFalse(type_id == TypeId('tfsr', flags={'fzht'}))
+        type_id = TypeId(name='*', flags={'cube'})
+        self.assertFalse(type_id == TypeId('geodataframe'))
+        self.assertFalse(type_id == TypeId('geodataframe', flags={'cube'}))
+        self.assertFalse(type_id == TypeId('dataset'))
+        self.assertFalse(type_id == TypeId('dataset', flags={'multilevel'}))
+        self.assertFalse(type_id == TypeId('dataset', flags={'multilevel', 'cube'}))
+        self.assertFalse(type_id == TypeId('dataset', flags={'cube'}))
         self.assertFalse(type_id == TypeId('*'))
-        self.assertTrue(type_id == TypeId('*', flags={'fzht'}))
+        self.assertTrue(type_id == TypeId('*', flags={'cube'}))
 
     def test_is_compatible(self):
-        type_id = TypeId(name='tfsr', flags={'fzht'})
-        self.assertFalse(type_id.is_compatible(TypeId('dtftg')))
-        self.assertFalse(type_id.is_compatible(TypeId('dtftg', flags={'fzht'})))
-        self.assertFalse(type_id.is_compatible(TypeId('tfsr')))
-        self.assertFalse(type_id.is_compatible(TypeId('tfsr', flags={'dsf'})))
-        self.assertTrue(type_id.is_compatible(TypeId('tfsr', flags={'dsf', 'fzht'})))
-        self.assertTrue(type_id.is_compatible(TypeId('tfsr', flags={'fzht'})))
+        type_id = TypeId(name='dataset', flags={'cube'})
+        self.assertFalse(type_id.is_compatible(TypeId('geodataframe')))
+        self.assertFalse(type_id.is_compatible(TypeId('geodataframe', flags={'cube'})))
+        self.assertFalse(type_id.is_compatible(TypeId('dataset')))
+        self.assertFalse(type_id.is_compatible(TypeId('dataset', flags={'multilevel'})))
+        self.assertTrue(type_id.is_compatible(TypeId('dataset', flags={'multilevel', 'cube'})))
+        self.assertTrue(type_id.is_compatible(TypeId('dataset', flags={'cube'})))
 
     def test_is_compatible_any(self):
-        type_id = TypeId(name='*', flags={'fzht'})
-        self.assertFalse(type_id.is_compatible(TypeId('dtftg')))
-        self.assertTrue(type_id.is_compatible(TypeId('dtftg', flags={'fzht'})))
-        self.assertFalse(type_id.is_compatible(TypeId('tfsr')))
-        self.assertFalse(type_id.is_compatible(TypeId('tfsr', flags={'dsf'})))
-        self.assertTrue(type_id.is_compatible(TypeId('tfsr', flags={'fzht'})))
-        self.assertTrue(type_id.is_compatible(TypeId('tfsr', flags={'dsf', 'fzht'})))
+        type_id = TypeId(name='*', flags={'cube'})
+        self.assertFalse(type_id.is_compatible(TypeId('geodataframe')))
+        self.assertTrue(type_id.is_compatible(TypeId('geodataframe', flags={'cube'})))
+        self.assertFalse(type_id.is_compatible(TypeId('dataset')))
+        self.assertFalse(type_id.is_compatible(TypeId('dataset', flags={'multilevel'})))
+        self.assertTrue(type_id.is_compatible(TypeId('dataset', flags={'cube'})))
+        self.assertTrue(type_id.is_compatible(TypeId('dataset', flags={'multilevel', 'cube'})))
 
     def test_normalize(self):
-        tfsr_flagged_id = TypeId(name='tfsr', flags={'fzht'})
-        self.assertEqual(TypeId.normalize(tfsr_flagged_id), tfsr_flagged_id)
-        self.assertEqual(TypeId.normalize('tfsr[fzht]'), tfsr_flagged_id)
+        dataset_flagged_id = TypeId(name='dataset', flags={'cube'})
+        self.assertEqual(TypeId.normalize(dataset_flagged_id), dataset_flagged_id)
+        self.assertEqual(TypeId.normalize('dataset[cube]'), dataset_flagged_id)
 
-        tfsr_id = TypeId(name='tfsr')
-        self.assertEqual(TypeId.normalize(tfsr_id), tfsr_id)
-        self.assertEqual(TypeId.normalize('tfsr'), tfsr_id)
+        dataset_id = TypeId(name='dataset')
+        self.assertEqual(TypeId.normalize(dataset_id), dataset_id)
+        self.assertEqual(TypeId.normalize('dataset'), dataset_id)
 
     def test_parse(self):
-        parsed_id = TypeId.parse('tfsr[fzht,rer]')
-        self.assertEqual('tfsr', parsed_id.name)
-        self.assertEqual({'fzht', 'rer'}, parsed_id.flags)
+        parsed_id = TypeId.parse('dataset[cube,multilevel]')
+        self.assertEqual('dataset', parsed_id.name)
+        self.assertEqual({'cube', 'multilevel'}, parsed_id.flags)
 
     def test_parse_exception(self):
         with self.assertRaises(SyntaxError) as cm:
