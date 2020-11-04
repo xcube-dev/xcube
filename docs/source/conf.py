2fb0ff4105ab86896e3bf8b3bc41572f577f4f6a
AliceBalfanz <alicja.balfanz@brockmann-consult.de>
2020-11-04 11:46:12 +0100

    try to fix docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -71,7 +71,7 @@ intersphinx_mapping = {
     'matplotlib': ('http://matplotlib.org/', None),
     'numpy': ('http://docs.scipy.org/doc/numpy/', None),
     'pandas': ('http://pandas.pydata.org/pandas-docs/stable/', None),
-    'python': ('https://docs.python.org/3.5/', None),
+    'python': ('https://docs.python.org/3/', None),
     'xarray': ('http://xarray.pydata.org/en/stable/', None),
     'zarr': ('https://zarr.readthedocs.io/en/stable/', None),
 }
