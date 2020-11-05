13fe28ecaa43bdc719878cdb42aafcd005492c85
AliceBalfanz <alicja.balfanz@brockmann-consult.de>
2020-11-05 09:06:39 +0100

    cleaning up

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -64,13 +64,6 @@ extensions = [
     'recommonmark',
 ]
 
-# extensions = [
-#     'sphinx.ext.autodoc',
-#     'sphinx.ext.coverage',
-#     'sphinx.ext.napoleon',
-#     'sphinx_rtd_theme',
-#     'recommonmark',
-# ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
