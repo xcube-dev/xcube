c84edc8063cd2a58a17fd51f98c1acb718df3455
AliceBalfanz <alicja.balfanz@brockmann-consult.de>
2020-11-05 08:58:40 +0100

    adjusting to be more like cates docu

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -51,12 +51,26 @@ source_suffix = ['.rst', '.md']
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
+    'sphinx.ext.doctest',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.todo',
     'sphinx.ext.coverage',
-    'sphinx.ext.napoleon',
-    'sphinx_rtd_theme',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.ifconfig',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.autodoc',
+    'sphinx_autodoc_annotation',
+    'sphinxarg.ext',
     'recommonmark',
 ]
+
+# extensions = [
+#     'sphinx.ext.autodoc',
+#     'sphinx.ext.coverage',
+#     'sphinx.ext.napoleon',
+#     'sphinx_rtd_theme',
+#     'recommonmark',
+# ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
