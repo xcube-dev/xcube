1c79158ee83eb44f9b41bfc7122edc31e9f25e23
Norman Fomferra <norman.fomferra@gmail.com>
2021-02-01 15:40:11 +0100

    fix

--- test/core/test_select.py
+++ test/core/test_select.py
@@ -53,7 +53,7 @@ class SelectSpatialSubsetTest(unittest.TestCase):
     def test_select_spatial_subset_some_xy_bbox(self):
         ds1 = create_highroc_dataset()
         ds2 = select_spatial_subset(ds1, xy_bbox=(8., 55, 10., 56.))
-        self.assertEqual((2, 2), ds2.conc_chl.shape)
+        self.assertEqual((3, 3), ds2.conc_chl.shape)
 
     def test_select_spatial_subset_none_xy_bbox(self):
         ds1 = create_highroc_dataset()
