18a34507073cfa9b3284a299d65e67b419f41555
Norman Fomferra <norman.fomferra@gmail.com>
2021-02-11 17:33:23 +0100

    Closes #375

--- test/core/test_reproject.py
+++ test/core/test_reproject.py
@@ -5,59 +5,13 @@ import numpy as np
 from matplotlib import pyplot as plt
 from numpy.testing import assert_array_almost_equal
 
-from test.sampledata import create_highroc_dataset, create_s2plus_dataset
-from xcube.core.reproject import reproject_xy_to_wgs84, reproject_crs_to_wgs84, get_projection_wkt
+from test.sampledata import create_highroc_dataset
+from xcube.core.reproject import reproject_xy_to_wgs84
 
 nan = np.nan
 
 
 class ReprojectTest(unittest.TestCase):
-
-    def test_reproject_crs_to_wgs84_s2plus(self):
-        dst_width = 6
-        dst_height = 4
-
-        dataset = create_s2plus_dataset()
-        proj_params = dataset.transverse_mercator.attrs
-        wkt = get_projection_wkt("Some S2+ Tile",
-                                 "Transverse_Mercator",
-                                 latitude_of_origin=proj_params["latitude_of_projection_origin"],
-                                 central_meridian=proj_params["longitude_of_central_meridian"],
-                                 scale_factor=proj_params["scale_factor_at_central_meridian"],
-                                 false_easting=proj_params["false_easting"],
-                                 false_northing=proj_params["false_northing"])
-
-        proj_dataset = reproject_crs_to_wgs84(dataset,
-                                              src_projection=wkt,
-                                              dst_region=(
-                                                  0.2727627754211426, 51.3291015625,
-                                                  0.273336261510849, 51.329463958740234,
-                                              ),
-                                              dst_size=(dst_width, dst_height))
-
-        self.assertIsNotNone(proj_dataset)
-        self.assertEqual(dict(lon=dst_width, lat=dst_height, bnds=2),
-                         proj_dataset.sizes)
-
-        self.assertIn('lon', proj_dataset)
-        self.assertEqual(proj_dataset.lon.shape, (dst_width,))
-        self.assertIn('lat', proj_dataset)
-        self.assertEqual(proj_dataset.lat.shape, (dst_height,))
-
-        self.assertIn('lon_bnds', proj_dataset)
-        self.assertEqual(proj_dataset.lon_bnds.shape, (dst_width, 2))
-        self.assertIn('lat_bnds', proj_dataset)
-        self.assertEqual(proj_dataset.lat_bnds.shape, (dst_height, 2))
-
-        expected_rrs_665 = np.array([[0.025002, 0.019001, 0.019001, 0.008999, 0.012001, 0.012001],
-                                     [0.028, 0.021, 0.021, 0.009998, 0.008999, 0.008999],
-                                     [0.036999, 0.022999, 0.022999, 0.007, 0.009998, 0.009998],
-                                     [0.033001, 0.018002, 0.018002, 0.007999, 0.008999, 0.008999]],
-                                    dtype=np.float32)
-        self.assertIn('rrs_665', proj_dataset)
-        self.assertEqual((dst_height, dst_width), proj_dataset.rrs_665.shape)
-        self.assertEqual(np.float32, proj_dataset.rrs_665.dtype)
-        assert_array_almost_equal(expected_rrs_665, proj_dataset.rrs_665)
 
     def test_reproject_xy_to_wgs84_highroc(self):
         dst_width = 12
