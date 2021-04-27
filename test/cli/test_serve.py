32d18f75a1019fc4d5bbeb71f2b8cf5efa596835
Norman Fomferra <norman.fomferra@gmail.com>
2021-04-27 14:01:03 +0200

    test update

--- test/cli/test_serve.py
+++ test/cli/test_serve.py
@@ -51,26 +51,27 @@ class ServerCliTest(CliDataTest):
 
         viewer_path = os.environ.get(VIEWER_ENV_VAR)
 
-        if viewer_path is not None:
+        if VIEWER_ENV_VAR in os.environ:
             del os.environ[VIEWER_ENV_VAR]
 
-        result = self.invoke_cli(["serve", "--show", "--config", config_path])
-        self.assertEqual(2, result.exit_code)
-        self.assertEqual(f'Error: Option "--show": In order to run the viewer, '
-                         f'set environment variable {VIEWER_ENV_VAR} to a valid '
-                         f'xcube-viewer deployment or build directory.\n',
-                         result.output[-150:])
+        try:
+            result = self.invoke_cli(["serve", "--show", "--config", config_path])
+            self.assertEqual(2, result.exit_code)
+            self.assertEqual(f'Error: Option "--show": In order to run the viewer, '
+                             f'set environment variable {VIEWER_ENV_VAR} to a valid '
+                             f'xcube-viewer deployment or build directory.\n',
+                             result.output[-150:])
 
-        os.environ[VIEWER_ENV_VAR] = "pip/po"
-        result = self.invoke_cli(["serve", "--show", "--config", config_path])
-        self.assertEqual(2, result.exit_code)
-        self.assertEqual(f'Error: Option "--show": Viewer path set by environment '
-                         f'variable {VIEWER_ENV_VAR} '
-                         f'must be a directory: pip/po\n',
-                         result.output[-110:])
-
-        if viewer_path is not None:
-            os.environ[VIEWER_ENV_VAR] = viewer_path
+            os.environ[VIEWER_ENV_VAR] = "pip/po"
+            result = self.invoke_cli(["serve", "--show", "--config", config_path])
+            self.assertEqual(2, result.exit_code)
+            self.assertEqual(f'Error: Option "--show": Viewer path set by environment '
+                             f'variable {VIEWER_ENV_VAR} '
+                             f'must be a directory: pip/po\n',
+                             result.output[-110:])
+        finally:
+            if viewer_path is not None:
+                os.environ[VIEWER_ENV_VAR] = viewer_path
 
     def test_help(self):
         with self.assertRaises(SystemExit):
