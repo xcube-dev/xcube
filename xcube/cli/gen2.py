12f26a9a80ed966723fc7ea666c4cd780c212ef1
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2021-09-01 16:36:05 +0200

    Added -o option to "xcube gen2" CLI

--- xcube/cli/gen2.py
+++ xcube/cli/gen2.py
@@ -19,7 +19,9 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+import json
 import sys
+import traceback
 from typing import Sequence
 
 import click
@@ -44,6 +46,11 @@ import click
               is_flag=True,
               help='Output information about the data cube to be generated.'
                    ' Does not generate the data cube.')
+@click.option('--output', '-o', 'output_file',
+              metavar='RESULT',
+              help='Write cube information or generation result into JSON'
+                   ' file RESULT.'
+                   ' If omitted, the JSON is dumped to stdout.')
 @click.option('--verbose', '-v',
               is_flag=True,
               multiple=True,
@@ -53,6 +60,7 @@ import click
 def gen2(request_path: str,
          stores_config_path: str = None,
          service_config_path: str = None,
+         output_file: str = None,
          info: bool = False,
          verbose: Sequence[bool] = None):
     """
@@ -115,47 +123,53 @@ def gen2(request_path: str,
     from xcube.core.gen2 import CubeGenerator
     from xcube.core.gen2 import CubeGeneratorError
     from xcube.core.gen2 import CubeGeneratorRequest
-    from xcube.core.gen2 import CubeInfo
-    from xcube.core.store import DataStoreError
 
     verbosity = len(verbose) if verbose else 0
 
+    error = None
+
+    # noinspection PyBroadException
     try:
         request = CubeGeneratorRequest.from_file(request_path,
                                                  verbosity=verbosity)
-
         generator = CubeGenerator.new(
             stores_config=stores_config_path,
             service_config=service_config_path,
             verbosity=verbosity
         )
+
         if info:
-            def dump_cube_info(cube_info: CubeInfo):
-                import sys
-                import json
-                json.dump(cube_info.to_dict(), sys.stdout, indent=2)
-
-            dump_cube_info(generator.get_cube_info(request))
+            result = generator.get_cube_info(request).to_dict()
         else:
-            generator.generate_cube(request)
+            result = generator.generate_cube(request).to_dict()
+    except BaseException as e:
+        error = e
 
-    except CubeGeneratorError as e:
-        print_kwargs = dict(file=sys.stderr, flush=True)
-        if e.remote_output:
-            print(**print_kwargs)
-            print('Remote output:', **print_kwargs)
-            print('==============', **print_kwargs)
-            for line in e.remote_output:
-                print(line, file=sys.stderr, flush=True)
-        if e.remote_traceback:
-            print(**print_kwargs)
-            print('Remote traceback:', **print_kwargs)
-            print('=================', **print_kwargs)
-            print(e.remote_traceback, **print_kwargs)
-        raise click.ClickException(f'{e}') from e
+        result = dict(status='error',
+                      message=f'{error}',
+                      traceback=traceback.format_tb(error.__traceback__))
+        if isinstance(error, CubeGeneratorError):
+            if error.remote_output:
+                result.update(remote_output=error.remote_output)
+            if error.remote_traceback:
+                result.update(remote_traceback=error.remote_traceback)
 
-    except DataStoreError as e:
-        raise click.ClickException(f'{e}') from e
+    if output_file is not None:
+        with open(output_file, 'w') as fp:
+            json.dump(result, fp, indent=2)
+        if verbosity:
+            print(f'Result written to {output_file}')
+    else:
+        json.dump(result, sys.stdout, indent=2)
+
+    if result.get('status') == 'error':
+        message = result.get('message',
+                             'Cube generation failed.'
+                             if error is None else f'{error}')
+        click_error = click.ClickException(message)
+        if error is not None:
+            raise click_error from error
+        raise click_error
 
 
 if __name__ == '__main__':
