67e9186c37fe3fc038202cee715d4fe03bb9819a
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2021-09-17 09:03:25 +0200

    name changes

--- test/util/test_versions.py
+++ test/util/test_versions.py
@@ -3,9 +3,9 @@ from xcube.util.versions import get_xcube_versions
 import xcube.version as xcube_version
 
 
-class GetDependenciesTest(unittest.TestCase):
+class GetVersionsTest(unittest.TestCase):
 
-    def test_get_xcube_versionss(self):
+    def test_get_xcube_versions(self):
         versions = get_xcube_versions()
         self.assertIsNotNone(versions)
         self.assertTrue('xarray' in versions)
