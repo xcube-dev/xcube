b6a949e94d203808e14f46abf729f976c7b02367
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2022-03-02 10:12:50 +0100

    Formatting to 78 chars line length

--- test/webapi/test_auth.py
+++ test/webapi/test_auth.py
@@ -1,6 +1,7 @@
 import json
-import unittest
 import os
+import unittest
+
 import requests
 
 from xcube.webapi.auth import AuthMixin
@@ -16,13 +17,17 @@ class RequestMock:
     def __init__(self, headers):
         self.headers = headers
 
+
 XCUBE_TEST_CLIENT_ID = os.environ.get('XCUBE_TEST_CLIENT_ID')
 XCUBE_TEST_CLIENT_SECRET = os.environ.get('XCUBE_TEST_CLIENT_SECRET')
 
+
 class AuthMixinIdTokenTest(unittest.TestCase):
 
-    @unittest.skipUnless(XCUBE_TEST_CLIENT_ID and XCUBE_TEST_CLIENT_SECRET,
-                         'XCUBE_TEST_CLIENT_ID and XCUBE_TEST_CLIENT_SECRET must be set')
+    @unittest.skipUnless(
+        XCUBE_TEST_CLIENT_ID and XCUBE_TEST_CLIENT_SECRET,
+        'XCUBE_TEST_CLIENT_ID and XCUBE_TEST_CLIENT_SECRET must be set'
+    )
     def test_ok(self):
         access_token = self._fetch_access_token()
 
@@ -32,16 +37,28 @@ class AuthMixinIdTokenTest(unittest.TestCase):
                 Domain='xcube-dev.eu.auth0.com',
                 Audience='https://xcube-dev/api/'
             )))
-        auth_mixin.request = RequestMock(headers={'Authorization': f'Bearer {access_token}'})
+        auth_mixin.request = RequestMock(
+            headers={'Authorization': f'Bearer {access_token}'}
+        )
 
         id_token = auth_mixin.get_id_token()
-        self.assertEqual('https://xcube-dev.eu.auth0.com/', id_token.get('iss'))
-        self.assertEqual('https://xcube-dev/api/', id_token.get('aud'))
-        self.assertEqual('w2NoDEryIpNRtAQVH1ToU6XTurE549FW', id_token.get('azp'))
-        self.assertEqual('client-credentials', id_token.get('gty'))
-        self.assertEqual('w2NoDEryIpNRtAQVH1ToU6XTurE549FW@clients', id_token.get('sub'))
-        self.assertEqual(['read:datasets'], id_token.get('permissions'))
-        self.assertEqual(86400, id_token.get('exp') - id_token.get('iat'))
+        self.assertEqual('https://xcube-dev.eu.auth0.com/',
+                         id_token.get('iss'))
+        self.assertEqual('https://xcube-dev/api/',
+                         id_token.get('aud'))
+        self.assertEqual('w2NoDEryIpNRtAQVH1ToU6XTurE549FW',
+                         id_token.get('azp'))
+        self.assertEqual('client-credentials',
+                         id_token.get('gty'))
+        self.assertEqual('w2NoDEryIpNRtAQVH1ToU6XTurE549FW@clients',
+                         id_token.get('sub'))
+        self.assertEqual(['read:datasets'],
+                         id_token.get('permissions'))
+        exp = id_token.get('exp')
+        iat = id_token.get('iat')
+        self.assertIsInstance(exp, int)
+        self.assertIsInstance(iat, int)
+        self.assertEqual(86400, exp - iat)
 
     def _fetch_access_token(self):
         response = requests.post(
@@ -62,10 +79,11 @@ class AuthMixinIdTokenTest(unittest.TestCase):
 
 
     def test_missing_auth_config(self):
-
         auth_mixin = AuthMixin()
         auth_mixin.service_context = ServiceContextMock(config={})
-        auth_mixin.request = RequestMock(headers={'Authorization': f'Bearer my_t0k3n'})
+        auth_mixin.request = RequestMock(
+            headers={'Authorization': f'Bearer my_t0k3n'}
+        )
 
         with self.assertRaises(ServiceAuthError) as cm:
             id_token = auth_mixin.get_id_token(require_auth=True)
@@ -79,8 +97,10 @@ class AuthMixinIdTokenTest(unittest.TestCase):
         auth_mixin.request = RequestMock(headers={})
         self.assertEqual(None, auth_mixin.get_id_token())
 
-    @unittest.skipUnless(XCUBE_TEST_CLIENT_ID and XCUBE_TEST_CLIENT_SECRET,
-                         'XCUBE_TEST_CLIENT_ID and XCUBE_TEST_CLIENT_SECRET must be set')
+    # @unittest.skipUnless(
+    #     XCUBE_TEST_CLIENT_ID and XCUBE_TEST_CLIENT_SECRET,
+    #     'XCUBE_TEST_CLIENT_ID and XCUBE_TEST_CLIENT_SECRET must be set'
+    # )
     def test_expired_access_token(self):
         auth_mixin = AuthMixin()
         auth_mixin.service_context = ServiceContextMock(config=dict(
@@ -89,26 +109,40 @@ class AuthMixinIdTokenTest(unittest.TestCase):
                 Audience='https://xcube-dev/api/'
             )))
 
-        expired_token = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik5VSkdSVUV5UWpNeE16UTVRVGMzTnpRM05URkNRa1ky" \
-                        "TURGRE5qQkJSak5ETlRBeVFrWXdOUSJ9.eyJpc3MiOiJodHRwczovL3hjdWJlLWRldi5ldS5hdXRoMC5jb20vIiw" \
-                        "ic3ViIjoidzJOb0RFcnlJcE5SdEFRVkgxVG9VNlhUdXJFNTQ5RldAY2xpZW50cyIsImF1ZCI6Imh0dHBzOi8veGN" \
-                        "1YmUtZGV2L2FwaS8iLCJpYXQiOjE1Nzk0NTUxMDMsImV4cCI6MTU3OTU0MTUwMywiYXpwIjoidzJOb0RFcnlJcE5" \
-                        "SdEFRVkgxVG9VNlhUdXJFNTQ5RlciLCJndHkiOiJjbGllbnQtY3JlZGVudGlhbHMiLCJwZXJtaXNzaW9ucyI6W11" \
-                        "9.NtGuVp0yY8foR_eFiEmH0eXvCx85cvb5b1cPWCKs6LCNniUJJ3VnX5Fek08puW7Jidy-tj1UTzyG569dSHGDu3" \
-                        "10Mf7xpQ9gyfZCWcaohERxsv9MrxHziqfGVnxv051rOB_c-fYyymrFnlsIqWeahcS7znvPoMovPO7E8MxVaIqxd_" \
-                        "S4zKVlH025F-bDvytuwXD-rFmYVElCg7u2uOZKqjpF3lZCWc50_F1jSGcEQZv4daQJY-3lfU6TnEQAuGWlOVRrCN" \
-                        "u05nlUBFPz6G82tB_nsP1uTa8uElOzoalVttXufLIeU0FL8Sv-lC6wUJTZAFpykLNmpA-vhkSeTqMv4g"
-        auth_mixin.request = RequestMock(headers={'Authorization': f'Bearer {expired_token}'})
+        expired_token = \
+            "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6" \
+            "Ik5VSkdSVUV5UWpNeE16UTVRVGMzTnpRM05URkNRa1ky" \
+            "TURGRE5qQkJSak5ETlRBeVFrWXdOUSJ9.eyJpc3MiOiJ" \
+            "odHRwczovL3hjdWJlLWRldi5ldS5hdXRoMC5jb20vIiw" \
+            "ic3ViIjoidzJOb0RFcnlJcE5SdEFRVkgxVG9VNlhUdXJ" \
+            "FNTQ5RldAY2xpZW50cyIsImF1ZCI6Imh0dHBzOi8veGN" \
+            "1YmUtZGV2L2FwaS8iLCJpYXQiOjE1Nzk0NTUxMDMsImV" \
+            "4cCI6MTU3OTU0MTUwMywiYXpwIjoidzJOb0RFcnlJcE5" \
+            "SdEFRVkgxVG9VNlhUdXJFNTQ5RlciLCJndHkiOiJjbGl" \
+            "lbnQtY3JlZGVudGlhbHMiLCJwZXJtaXNzaW9ucyI6W11" \
+            "9.NtGuVp0yY8foR_eFiEmH0eXvCx85cvb5b1cPWCKs6L" \
+            "CNniUJJ3VnX5Fek08puW7Jidy-tj1UTzyG569dSHGDu3" \
+            "10Mf7xpQ9gyfZCWcaohERxsv9MrxHziqfGVnxv051rOB" \
+            "_c-fYyymrFnlsIqWeahcS7znvPoMovPO7E8MxVaIqxd_" \
+            "S4zKVlH025F-bDvytuwXD-rFmYVElCg7u2uOZKqjpF3l" \
+            "ZCWc50_F1jSGcEQZv4daQJY-3lfU6TnEQAuGWlOVRrCN" \
+            "u05nlUBFPz6G82tB_nsP1uTa8uElOzoalVttXufLIeU0" \
+            "FL8Sv-lC6wUJTZAFpykLNmpA-vhkSeTqMv4g"
+        auth_mixin.request = RequestMock(
+            headers={'Authorization': f'Bearer {expired_token}'})
         with self.assertRaises(ServiceAuthError) as cm:
             auth_mixin.get_id_token()
-        self.assertEqual('HTTP 401: Token expired (Token is expired)', f'{cm.exception}')
+        self.assertEqual('HTTP 401: Token expired (Token is expired)',
+                         f'{cm.exception}')
 
 
 class AuthMixinAccessTokenTest(unittest.TestCase):
 
     def test_ok(self):
         auth_mixin = AuthMixin()
-        auth_mixin.request = RequestMock(headers={'Authorization': 'Bearer my_t0k3n'})
+        auth_mixin.request = RequestMock(
+            headers={'Authorization': 'Bearer my_t0k3n'}
+        )
         self.assertEqual('my_t0k3n', auth_mixin.get_access_token())
 
     def test_ok_no_auth(self):
@@ -122,25 +156,34 @@ class AuthMixinAccessTokenTest(unittest.TestCase):
         auth_mixin.request = RequestMock(headers={})
         with self.assertRaises(ServiceAuthError) as cm:
             auth_mixin.get_access_token(require_auth=True)
-        self.assertEqual('HTTP 401: Authorization header missing (Authorization header is expected)',
+        self.assertEqual('HTTP 401: Authorization header'
+                         ' missing (Authorization header is expected)',
                          f'{cm.exception}')
 
-        auth_mixin.request = RequestMock(headers={'Authorization': 'Beerer my_t0k3n'})
+        auth_mixin.request = RequestMock(
+            headers={'Authorization': 'Beerer my_t0k3n'}
+        )
         with self.assertRaises(ServiceAuthError) as cm:
             auth_mixin.get_access_token()
-        self.assertEqual('HTTP 401: Invalid header (Authorization header must start with "Bearer")',
+        self.assertEqual('HTTP 401: Invalid header'
+                         ' (Authorization header must start with "Bearer")',
                          f'{cm.exception}')
 
-        auth_mixin.request = RequestMock(headers={'Authorization': 'Bearer'})
+        auth_mixin.request = RequestMock(
+            headers={'Authorization': 'Bearer'}
+        )
         with self.assertRaises(ServiceAuthError) as cm:
             auth_mixin.get_access_token()
         self.assertEqual('HTTP 401: Invalid header (Bearer token not found)',
                          f'{cm.exception}')
 
-        auth_mixin.request = RequestMock(headers={'Authorization': 'Bearer my t0k3n'})
+        auth_mixin.request = RequestMock(
+            headers={'Authorization': 'Bearer my t0k3n'}
+        )
         with self.assertRaises(ServiceAuthError) as cm:
             auth_mixin.get_access_token()
-        self.assertEqual('HTTP 401: Invalid header (Authorization header must be Bearer token)',
+        self.assertEqual('HTTP 401: Invalid header'
+                         ' (Authorization header must be Bearer token)',
                          f'{cm.exception}')
 
 
@@ -154,16 +197,21 @@ class AuthMixinAuthConfigTest(unittest.TestCase):
                 Audience='https://xcube-dev/api/'
             )))
         auth_config = auth_mixin.auth_config
-        self.assertEqual('xcube-dev.eu.auth0.com', auth_config.domain)
-        self.assertEqual('https://xcube-dev/api/', auth_config.audience)
-        self.assertEqual('https://xcube-dev.eu.auth0.com/', auth_config.issuer)
-        self.assertEqual('https://xcube-dev.eu.auth0.com/.well-known/jwks.json', auth_config.well_known_jwks)
+        self.assertEqual('xcube-dev.eu.auth0.com',
+                         auth_config.domain)
+        self.assertEqual('https://xcube-dev/api/',
+                         auth_config.audience)
+        self.assertEqual('https://xcube-dev.eu.auth0.com/',
+                         auth_config.issuer)
+        self.assertEqual('https://xcube-dev.eu.auth0.com/'
+                         '.well-known/jwks.json',
+                         auth_config.well_known_jwks)
         self.assertEqual(['RS256'], auth_config.algorithms)
 
     def test_not_ok(self):
-        class ServiceContextMock:
-            def __init__(self, config):
-                self.config = config
+        # class ServiceContextMock:
+        #     def __init__(self, config):
+        #         self.config = config
 
         auth_mixin = AuthMixin()
         auth_mixin.service_context = ServiceContextMock(config=dict(
@@ -174,7 +222,8 @@ class AuthMixinAuthConfigTest(unittest.TestCase):
         with self.assertRaises(ServiceConfigError) as cm:
             # noinspection PyUnusedLocal
             auth_config = auth_mixin.auth_config
-        self.assertEqual('HTTP 500: Missing key "Audience" in section "Authentication"',
+        self.assertEqual('HTTP 500: Missing key'
+                         ' "Audience" in section "Authentication"',
                          f'{cm.exception}')
 
         auth_mixin.service_context = ServiceContextMock(config=dict(
@@ -185,7 +234,8 @@ class AuthMixinAuthConfigTest(unittest.TestCase):
         with self.assertRaises(ServiceConfigError) as cm:
             # noinspection PyUnusedLocal
             auth_config = auth_mixin.auth_config
-        self.assertEqual('HTTP 500: Missing key "Domain" in section "Authentication"',
+        self.assertEqual('HTTP 500: Missing'
+                         ' key "Domain" in section "Authentication"',
                          f'{cm.exception}')
 
         auth_mixin.service_context = ServiceContextMock(config=dict(
@@ -198,5 +248,6 @@ class AuthMixinAuthConfigTest(unittest.TestCase):
         with self.assertRaises(ServiceConfigError) as cm:
             # noinspection PyUnusedLocal
             auth_config = auth_mixin.auth_config
-        self.assertEqual('HTTP 500: Value for key "Algorithms" in section "Authentication" must not be empty',
+        self.assertEqual('HTTP 500: Value for key "Algorithms"'
+                         ' in section "Authentication" must not be empty',
                          f'{cm.exception}')
--- xcube/webapi/auth.py
+++ xcube/webapi/auth.py
@@ -1,5 +1,23 @@
-"""Python Flask API Auth0 integration example
-"""
+# The MIT License (MIT)
+# Copyright (c) 2022 by the xcube development team and contributors
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of
+# this software and associated documentation files (the "Software"), to deal in
+# the Software without restriction, including without limitation the rights to
+# use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+# of the Software, and to permit persons to whom the Software is furnished to do
+# so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
 import json
 from typing import Optional, Mapping, List, Dict, Any, Set
@@ -7,6 +25,7 @@ from typing import Optional, Mapping, List, Dict, Any, Set
 import jwt
 import requests
 from jwt.algorithms import RSAAlgorithm
+
 from xcube.webapi.errors import ServiceAuthError, ServiceConfigError
 
 
@@ -43,13 +62,20 @@ class AuthConfig:
             return None
         domain = authentication.get('Domain')
         if not domain:
-            raise ServiceConfigError('Missing key "Domain" in section "Authentication"')
+            raise ServiceConfigError(
+                'Missing key "Domain" in section "Authentication"'
+            )
         audience = authentication.get('Audience')
         if not audience:
-            raise ServiceConfigError('Missing key "Audience" in section "Authentication"')
+            raise ServiceConfigError(
+                'Missing key "Audience" in section "Authentication"'
+            )
         algorithms = authentication.get('Algorithms', ['RS256'])
         if not algorithms:
-            raise ServiceConfigError('Value for key "Algorithms" in section "Authentication" must not be empty')
+            raise ServiceConfigError(
+                'Value for key "Algorithms" in section'
+                ' "Authentication" must not be empty'
+            )
         return AuthConfig(domain, audience, algorithms)
 
 
@@ -74,25 +100,34 @@ class AuthMixin:
         auth = self.request.headers.get("Authorization", None)
         if not auth:
             if require_auth:
-                raise ServiceAuthError("Authorization header missing",
-                                       log_message="Authorization header is expected")
+                raise ServiceAuthError(
+                    "Authorization header missing",
+                    log_message="Authorization header is expected"
+                )
             return None
 
         parts = auth.split()
 
         if parts[0].lower() != "bearer":
-            raise ServiceAuthError("Invalid header",
-                                   log_message='Authorization header must start with "Bearer"')
+            raise ServiceAuthError(
+                "Invalid header",
+                log_message='Authorization header must start with "Bearer"'
+            )
         elif len(parts) == 1:
-            raise ServiceAuthError("Invalid header",
-                                   log_message="Bearer token not found")
+            raise ServiceAuthError(
+                "Invalid header",
+                log_message="Bearer token not found"
+            )
         elif len(parts) > 2:
-            raise ServiceAuthError("Invalid header",
-                                   log_message="Authorization header must be Bearer token")
+            raise ServiceAuthError(
+                "Invalid header",
+                log_message="Authorization header must be Bearer token"
+            )
 
         return parts[1]
 
-    def get_id_token(self, require_auth: bool = False) -> Optional[Mapping[str, str]]:
+    def get_id_token(self, require_auth: bool = False) \
+            -> Optional[Mapping[str, str]]:
         """
         Decodes the access token is valid.
         """
@@ -103,19 +138,28 @@ class AuthMixin:
         auth_config = self.auth_config
         if auth_config is None:
             if require_auth:
-                raise ServiceAuthError("Invalid header",
-                                       log_message="Received access token, "
-                                                   "but this server doesn't support authentication.")
+                raise ServiceAuthError(
+                    "Invalid header",
+                    log_message="Received access token,"
+                                " but this server doesn't support"
+                                " authentication."
+                )
             return None
 
         try:
             unverified_header = jwt.get_unverified_header(access_token)
         except jwt.InvalidTokenError:
-            raise ServiceAuthError("Invalid header",
-                                   log_message="Invalid header. Use an RS256 signed JWT Access Token")
+            raise ServiceAuthError(
+                "Invalid header",
+                log_message="Invalid header."
+                            " Use an RS256 signed JWT Access Token"
+            )
         if unverified_header["alg"] != "RS256":  # e.g. "HS256"
-            raise ServiceAuthError("Invalid header",
-                                   log_message="Invalid header. Use an RS256 signed JWT Access Token")
+            raise ServiceAuthError(
+                "Invalid header",
+                log_message="Invalid header."
+                            " Use an RS256 signed JWT Access Token"
+            )
 
         # TODO: read jwks from cache
         response = requests.get(auth_config.well_known_jwks)
@@ -137,21 +181,29 @@ class AuthMixin:
             try:
                 id_token = jwt.decode(
                     access_token,
-                    # TODO: this is stupid: we convert rsa_key to JWT JSON only to produce the public key JSON string
+                    # TODO: this is stupid: we convert rsa_key to
+                    #  JWT JSON only to produce the public key JSON string
                     RSAAlgorithm.from_jwk(json.dumps(rsa_key)),
                     algorithms=auth_config.algorithms,
                     audience=auth_config.audience,
                     issuer=auth_config.issuer
                 )
             except jwt.ExpiredSignatureError:
-                raise ServiceAuthError("Token expired",
-                                       log_message="Token is expired")
+                raise ServiceAuthError(
+                    "Token expired",
+                    log_message="Token is expired"
+                )
             except jwt.InvalidTokenError:
-                raise ServiceAuthError("Invalid claims",
-                                       log_message="Incorrect claims, please check the audience and issuer")
+                raise ServiceAuthError(
+                    "Invalid claims",
+                    log_message="Incorrect claims,"
+                                " please check the audience and issuer"
+                )
             except Exception:
-                raise ServiceAuthError("Invalid header",
-                                       log_message="Unable to parse authentication token.")
+                raise ServiceAuthError(
+                    "Invalid header",
+                    log_message="Unable to parse authentication token."
+                )
             return id_token
 
         raise ServiceAuthError("Invalid header",
@@ -162,14 +214,18 @@ def assert_scopes(required_scopes: Set[str],
                   granted_scopes: Set[str]):
     """
     Assert scopes.
-    Raise ServiceAuthError if one of *required_scopes* is not in *granted_scopes*.
+    Raise ServiceAuthError if one of *required_scopes* is
+    not in *granted_scopes*.
 
     :param required_scopes: The list of required scopes
     :param granted_scopes: The set of granted scopes
     """
     missing_scope = _get_missing_scope(required_scopes, granted_scopes)
     if missing_scope is not None:
-        raise ServiceAuthError('Missing permission', log_message=f'Missing permission {missing_scope}')
+        raise ServiceAuthError(
+            'Missing permission',
+            log_message=f'Missing permission {missing_scope}'
+        )
 
 
 def check_scopes(required_scopes: Set[str],
@@ -178,7 +234,8 @@ def check_scopes(required_scopes: Set[str],
     """
     Check scopes.
 
-    This function is used to filter out a resource's sub-resources for which a given client has no permission.
+    This function is used to filter out a resource's sub-resources for
+    which a given client has no permission.
 
     If one of *required_scopes* is not in *granted_scopes*, fail.
     If *granted_scopes* exists and *is_substitute*, fail too.
@@ -186,10 +243,13 @@ def check_scopes(required_scopes: Set[str],
 
     :param required_scopes: The list of required scopes
     :param granted_scopes: The set of granted scopes
-    :param is_substitute: True, if the resource to be checked is a substitute.
+    :param is_substitute: True, if the resource to be checked
+        is a substitute.
     :return: True, if scopes are ok.
     """
-    return _get_missing_scope(required_scopes, granted_scopes, is_substitute=is_substitute) is None
+    return _get_missing_scope(required_scopes,
+                              granted_scopes,
+                              is_substitute=is_substitute) is None
 
 
 def _get_missing_scope(required_scopes: Set[str],
@@ -200,8 +260,9 @@ def _get_missing_scope(required_scopes: Set[str],
             # If the required scope is not a granted scope, fail
             return required_scope
     # If there are granted scopes then the client is authorized,
-    # hence fail for substitute resources (e.g. demo resources) as there is usually
-    # a better (non-demo) resource that replaces it.
+    # hence fail for substitute resources (e.g. demo resources)
+    # as there is usually a better (non-demo) resource that
+    # replaces it.
     if granted_scopes and is_substitute:
         return '<is_substitute>'
     # All ok.
