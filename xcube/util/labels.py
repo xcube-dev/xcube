1545510c14f4bceb6ecaa2ebb4579146e30aebe4
Pontus Lurcock <pontus.lurcock@brockmann-consult.de>
2022-04-01 17:23:48 +0200

    util.labels.ensure_time_compatible improvements
     - Handle slices correctly
    
     - Refactoring to make logic clearer
    
     - Expand docstring

--- xcube/util/labels.py
+++ xcube/util/labels.py
@@ -30,21 +30,54 @@ def ensure_time_compatible(var: xr.DataArray,
                            labels: Dict[str, Any]) -> Dict[str, Any]:
     """Ensure that labels['time'] is timezone-naive, if necessary.
 
+    This function returns either the passed-in labels object, or a copy of
+    it with a modified value for labels['time'].
+
+    If there is no 'time' key in the labels dictionary or if there is no
+    'time' dimension in the var array, the original labels are returned.
+
+    If there is a 'time' key, it is expected that its value will be
+    a valid timestamp (i.e. a valid input to pandas.Timestamp.__init__), or
+    a slice in which the start and stop fields are valid timestamps. For a
+    slice, the start and stop fields are processed separately, and their
+    modified values (if required) are returned as the start and stop fields
+    of a new slice. The step field is included unchanged in the new slice.
+
     If var has a 'time' dimension of type datetime64 and labels has a 'time'
     key with a timezone-aware value, return a modified labels dictionary with
     a timezone-naive time value. Otherwise return the original labels.
+
     """
-    if _has_datetime64_time(var) and \
-       'time' in labels and pd.Timestamp(labels['time']).tzinfo is not None:
-        naive_time = pd.Timestamp(labels['time']).tz_convert(None)
-        return dict(labels, time=naive_time)
-    else:
+
+    if 'time' not in labels or 'time' not in var.dims:
         return labels
+
+    timeval = labels['time']
+    if isinstance(timeval, slice):
+        # process start and stop separately and pass step through unchanged
+        return dict(labels, time=slice(
+            _ensure_timestamp_compatible(var, timeval.start),
+            _ensure_timestamp_compatible(var, timeval.stop),
+            timeval.step))
+    else:
+        return dict(labels, time=_ensure_timestamp_compatible(var, timeval))
+
+
+def _ensure_timestamp_compatible(var: xr.DataArray, timeval: Any):
+    timestamp = pd.Timestamp(timeval)
+    timeval_is_naive = timestamp.tzinfo is None
+    if _has_datetime64_time(var) and not timeval_is_naive:
+        # pandas treats datetime64s as timezone-naive, so we naivefy the label
+        naive_time = timestamp.tz_convert(None)
+        return naive_time
+    else:
+        return timeval
 
 
 def _has_datetime64_time(var: xr.DataArray) -> bool:
-    """Report whether var has a time dimension with type datetime64"""
-    return 'time' in var.dims and \
-           hasattr(var['time'], 'dtype') and \
-           hasattr(var['time'].dtype, 'type') and \
-           var['time'].dtype.type is np.datetime64
+    """Report whether var has a time dimension with type datetime64
+
+    Assumes a 'time' key is present in var.dims."""
+    return hasattr(var['time'], 'dtype') \
+        and hasattr(var['time'].dtype, 'type') \
+        and var['time'].dtype.type is np.datetime64
