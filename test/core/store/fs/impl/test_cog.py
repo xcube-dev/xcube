d7aa5b89faa3b6d260ffd54ca2b31770b11ebf97
tejas <tejas.morbagalharish@brockmann-consult.de>
2022-05-11 09:59:53 +0200

    Implemented a class for geotiff to be wrapped in a mldataset

--- test/core/store/fs/impl/test_cog.py
+++ test/core/store/fs/impl/test_cog.py
@@ -21,15 +21,20 @@
 
 import unittest
 
+import fsspec
 import rasterio as rio
 import rioxarray
 import xarray as xr
+
+from xcube.core.store.fs.impl.cog import GeoTIFFMultiLevelDataset
 
 
 class RioXarrayTest(unittest.TestCase):
 
     def test_it_is_possible_get_overviews(self):
-        array = rioxarray.open_rasterio('testdata/cog-example.tif')
+        cog_path = 'examples/serve/demo/cog-example.tif'
+        # array = rioxarray.open_rasterio('examples/serve/demo/cog-example.tif')
+        array = rioxarray.open_rasterio(cog_path)
         self.assertIsInstance(array, xr.DataArray)
         self.assertEqual((3, 9984, 22016), array.shape)
 
@@ -51,7 +56,7 @@ class RioXarrayTest(unittest.TestCase):
         num_levels = len(overviews)
         shapes = []
         for i in range(num_levels):
-            array = rioxarray.open_rasterio('testdata/cog-example.tif',
+            array = rioxarray.open_rasterio(cog_path,
                                             overview_level=i)
             shapes.append(array.shape)
 
@@ -64,9 +69,40 @@ class RioXarrayTest(unittest.TestCase):
                          shapes)
 
         with self.assertRaises(rio.errors.RasterioIOError) as e:
-            rioxarray.open_rasterio('testdata/cog-example.tif',
+            rioxarray.open_rasterio(cog_path,
                                     overview_level=num_levels)
         self.assertEqual(
-            ('Cannot open overview level 6 of testdata/cog-example.tif',),
+            (
+                'Cannot open overview level 6 of '
+                'examples/serve/demo/cog-example.tif',),
             e.exception.args
         )
+
+
+class GeoTIFFMultiLevelDatasetTest(unittest.TestCase):
+
+    def test_local_fs(self):
+        fs = fsspec.filesystem('file')
+        cog_path = "examples/serve/demo/cog-example.tif"
+        ml_dataset = GeoTIFFMultiLevelDataset(fs, None, cog_path)
+        self.assertEqual(7, ml_dataset.num_levels)
+        self.assertEqual(cog_path, ml_dataset.ds_id)
+        self.assertEqual([(0.03732275, 0.03732275),
+                          (0.0746455, 0.0746455),
+                          (0.149291, 0.149291),
+                          (0.298582, 0.298582),
+                          (0.597164, 0.597164),
+                          (1.194328, 1.194328),
+                          (2.388656, 2.388656)], ml_dataset.resolutions)
+        dataset = ml_dataset.get_dataset(0)
+        self.assertEqual(['band_1', 'band_2', 'band_3'],
+                         list(dataset.data_vars))
+        self.assertEqual((9984, 22016),
+                         dataset.band_1.shape)
+        dataset = ml_dataset.get_dataset(6)
+        self.assertEqual(['band_1', 'band_2', 'band_3'],
+                         list(dataset.data_vars))
+        self.assertEqual((9984 // 2 ** 6, 22016 // 2 ** 6),
+                         dataset.band_1.shape)
+        datasets = ml_dataset.datasets
+        self.assertEqual(7, len(datasets))
