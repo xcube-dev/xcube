eadc5648c4ca892afc233a7ff20e2b42ce45ecf2
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2022-05-24 08:21:51 +0200

    Fixed tests

--- test/core/gridmapping/test_dataset.py
+++ test/core/gridmapping/test_dataset.py
@@ -118,20 +118,22 @@ class DatasetGridMappingTest(unittest.TestCase):
 
         gm = GridMapping.from_dataset(dataset, prefer_is_regular=False)
         # Should pick the geographic one which is irregular
-        self.assertEqual('Geographic 2D CRS', gm.crs.type_name)
+        self.assertEqual('Derived Geographic 2D CRS', gm.crs.type_name)
         self.assertEqual(False, gm.is_regular)
 
         gm = GridMapping.from_dataset(dataset, prefer_crs=GEO_CRS)
         # Should pick the geographic one which is irregular
-        self.assertEqual('Geographic 2D CRS', gm.crs.type_name)
+        self.assertEqual('Derived Geographic 2D CRS', gm.crs.type_name)
         self.assertEqual(False, gm.is_regular)
 
-        gm = GridMapping.from_dataset(dataset, prefer_crs=GEO_CRS, prefer_is_regular=True)
+        gm = GridMapping.from_dataset(dataset, prefer_crs=GEO_CRS,
+                                      prefer_is_regular=True)
         # Should pick the geographic one which is irregular
-        self.assertEqual('Geographic 2D CRS', gm.crs.type_name)
+        self.assertEqual('Derived Geographic 2D CRS', gm.crs.type_name)
         self.assertEqual(False, gm.is_regular)
 
     def test_no_grid_mapping_found(self):
         with self.assertRaises(ValueError) as cm:
             GridMapping.from_dataset(xr.Dataset())
-        self.assertEqual('cannot find any grid mapping in dataset', f'{cm.exception}')
+        self.assertEqual('cannot find any grid mapping in dataset',
+                         f'{cm.exception}')
