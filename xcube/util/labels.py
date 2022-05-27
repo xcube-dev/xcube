6ef639d10b6fff4de0aa2d47b613667e62e7993d
Pontus Lurcock <pontus.lurcock@brockmann-consult.de>
2022-05-27 17:43:10 +0200

    Make indexer timezone checking more robust

--- xcube/util/labels.py
+++ xcube/util/labels.py
@@ -75,30 +75,40 @@ def _ensure_timestamp_compatible(var: xr.DataArray, timeval: Any):
     if timeval is None:
         return None
 
-    timestamp = pd.Timestamp(timeval)
-    timeval_timezone = timestamp.tzinfo
-    array_timezone = _get_array_timezone(var)
-
-    if array_timezone is None and timeval_timezone is not None:
-        return timestamp.tz_convert(None)
-    elif array_timezone is not None and timeval_timezone is None:
-        return timestamp.tz_localize(array_timezone)
+    if hasattr(timeval, "tzinfo"):
+        timestamp = timeval
+        timeval_tzinfo = timeval.tzinfo
     else:
+        try:
+            timestamp = pd.Timestamp(timeval)
+            timeval_tzinfo = timestamp.tzinfo
+        except TypeError:
+            logger.warning("Can't determine indexer timezone, leaving it "
+                           "unmodified.")
+            return timeval
+
+    if _has_datetime64_time(var):
+        # pandas treats all datetime64 arrays as timezone-naive
+        array_timezone = None
+    elif hasattr(var.time.values[0], 'tzinfo'):
+        array_timezone = var.time.values[0].tzinfo
+    else:
+        logger.warning(
+            "Can't determine array timezone, leaving indexer unmodified.")
         return timeval
 
-
-def _get_array_timezone(var: xr.DataArray):
-    # TODO: also check for non-datetime64 tz-naive arrays!
-
-    # pandas treats all datetime64 arrays as timezone-naive
-    if _has_datetime64_time(var):
-        return None
-
-    if isinstance(var.time.values[0], pd.Timestamp):
-        return var.time.values[0].tzinfo
-
-    logger.warning("Can't determine array timezone, assuming TZ-naive")
-    return None
+    if array_timezone is None and timeval_tzinfo is not None:
+        if hasattr(timestamp, 'tz_convert'):
+            return timestamp.tz_convert(None)
+        else:
+            logger.warning("Indexer lacks tz_convert, leaving unmodified")
+    elif array_timezone is not None and timeval_tzinfo is None:
+        if hasattr(timestamp, 'tz_localize'):
+            return timestamp.tz_localize(array_timezone)
+        else:
+            logger.warning("Indexer lacks tz_localize, leaving unmodified")
+    else:
+        return timeval
 
 
 def _has_datetime64_time(var: xr.DataArray) -> bool:
