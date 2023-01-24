d56a93f6b1be1f4800df349ffc27e41ad1e4fbac
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2023-01-24 15:26:34 +0100

    integrated xcube-viewer build

--- xcube/webapi/meta/routes.py
+++ xcube/webapi/meta/routes.py
@@ -48,7 +48,7 @@ class OpenApiHtmlHandler(ApiHandler):
     def get(self):
         include_all = self.request.get_query_arg('all', default=False)
         html_template = pkgutil.get_data('xcube.webapi.meta',
-                                         'res/openapi.html').decode('utf-8')
+                                         'data/openapi.html').decode('utf-8')
         self.response.finish(Template(html_template).substitute(
             open_api_url=self.request.url_for_path(
                 'openapi.json',
