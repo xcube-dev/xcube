2d195497aa8f4b9ec06abcf8c33aea60a862ed8d
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2023-02-01 17:20:18 +0100

    Apply suggestions from code review
    Co-authored-by: Alicja B. <23457217+AliceBalfanz@users.noreply.github.com>

--- test/webapi/viewer/test_routes.py
+++ test/webapi/viewer/test_routes.py
@@ -1,5 +1,5 @@
 # The MIT License (MIT)
-# Copyright (c) 2022 by the xcube team and contributors
+# Copyright (c) 2023 by the xcube team and contributors
 #
 # Permission is hereby granted, free of charge, to any person obtaining a
 # copy of this software and associated documentation files (the "Software"),
