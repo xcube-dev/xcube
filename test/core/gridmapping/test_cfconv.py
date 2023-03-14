a0e7ae272e318f544c67622b2a986274e57bc812
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2023-03-14 10:27:48 +0100

    cont. dev

--- test/core/gridmapping/test_cfconv.py
+++ test/core/gridmapping/test_cfconv.py
@@ -1,9 +1,13 @@
 import unittest
 
+import numpy as np
 import pyproj
+import xarray as xr
+
+from xcube.core.gridmapping import GridMapping
+from xcube.core.gridmapping.cfconv import find_grid_mapping_for_var
 
 CRS_WGS84 = pyproj.crs.CRS(4326)
-CRS_CRS84 = pyproj.crs.CRS.from_string("urn:ogc:def:crs:OGC:1.3:CRS84")
 CRS_UTM_33N = pyproj.crs.CRS(32633)
 
 CRS_ROTATED_POLE = pyproj.crs.CRS.from_cf(
@@ -12,6 +16,133 @@ CRS_ROTATED_POLE = pyproj.crs.CRS.from_cf(
          grid_north_pole_longitude=170.))
 
 
-class GetDatasetGridMappingTest(unittest.TestCase):
-    def test_it(self):
-        pass
+class FindGridMappingForVarTest(unittest.TestCase):
+    w = 20
+    h = 10
+
+    def new_data_var(self,
+                     shape=None,
+                     dims=None,
+                     attrs=None) -> xr.DataArray:
+        return xr.DataArray(
+            np.zeros(shape or (1, self.h, self.w), dtype=np.float32),
+            dims=dims or ("time", "y", "x"),
+            attrs=attrs
+        )
+
+    def new_x_coord_var(self,
+                        dim=None,
+                        attrs=None) -> xr.DataArray:
+        return xr.DataArray(
+            np.linspace(10000, 10000 + 10 * self.w, self.w),
+            dims=dim or "x",
+            attrs=attrs
+        )
+
+    def new_y_coord_var(self,
+                        dim=None,
+                        attrs=None) -> xr.DataArray:
+        return xr.DataArray(
+            np.linspace(10000, 10000 + 10 * self.h, self.h),
+            dims=dim or "y",
+            attrs=attrs
+        )
+
+    def new_crs_var(self, crs) -> xr.DataArray:
+        return xr.DataArray(0, attrs=crs.to_cf())
+
+    def test_with_grid_mapping_and_named_coords(self):
+        ds = xr.Dataset(
+            data_vars={
+                "sst": self.new_data_var(
+                    attrs={"grid_mapping": "crs: a b"}
+                ),
+                "crs": self.new_crs_var(CRS_UTM_33N)
+            },
+            coords={
+                "a": self.new_x_coord_var(),
+                "b": self.new_y_coord_var(),
+                "x": self.new_x_coord_var(),
+                "y": self.new_y_coord_var(),
+            }
+        )
+        gm = find_grid_mapping_for_var(ds, "sst")
+        self.assertIsInstance(gm, GridMapping)
+        self.assertEqual(CRS_UTM_33N, gm.crs)
+        self.assertEqual(("a", "b"), gm.xy_var_names)
+        self.assertEqual(("x", "y"), gm.xy_dim_names)
+
+    def test_with_grid_mapping_and_standard_name_lat_lon(self):
+        ds = xr.Dataset(
+            data_vars={
+                "sst": self.new_data_var(
+                    attrs={"grid_mapping": "crs"}
+                ),
+                "crs": self.new_crs_var(CRS_WGS84)
+            },
+            coords={
+                "a": self.new_x_coord_var(
+                    attrs={"standard_name": "longitude"}
+                ),
+                "b": self.new_y_coord_var(
+                    attrs={"standard_name": "latitude"}
+                ),
+                "lon": self.new_x_coord_var(),
+                "lat": self.new_y_coord_var(),
+            }
+        )
+        gm = find_grid_mapping_for_var(ds, "sst")
+        self.assertIsInstance(gm, GridMapping)
+        self.assertEqual(CRS_WGS84, gm.crs)
+        self.assertEqual(("a", "b"), gm.xy_var_names)
+        self.assertEqual(("x", "y"), gm.xy_dim_names)
+
+    def test_with_grid_mapping_and_standard_name_rot_lat_lon(self):
+        ds = xr.Dataset(
+            data_vars={
+                "sst": self.new_data_var(
+                    attrs={"grid_mapping": "crs"}
+                ),
+                "crs": self.new_crs_var(CRS_ROTATED_POLE)
+            },
+            coords={
+                "a": self.new_x_coord_var(
+                    attrs={"standard_name": "grid_longitude"}
+                ),
+                "b": self.new_y_coord_var(
+                    attrs={"standard_name": "grid_latitude"}
+                ),
+                "lon": self.new_x_coord_var(),
+                "lat": self.new_y_coord_var(),
+            }
+        )
+        gm = find_grid_mapping_for_var(ds, "sst")
+        self.assertIsInstance(gm, GridMapping)
+        self.assertEqual(CRS_ROTATED_POLE.to_cf(), gm.crs.to_cf())
+        self.assertEqual(("a", "b"), gm.xy_var_names)
+        self.assertEqual(("x", "y"), gm.xy_dim_names)
+
+    def test_with_grid_mapping_and_standard_name_projected(self):
+        ds = xr.Dataset(
+            data_vars={
+                "sst": self.new_data_var(
+                    attrs={"grid_mapping": "crs"}
+                ),
+                "crs": self.new_crs_var(CRS_UTM_33N)
+            },
+            coords={
+                "a": self.new_x_coord_var(
+                    attrs={"standard_name": "projection_x_coordinate"}
+                ),
+                "b": self.new_y_coord_var(
+                    attrs={"standard_name": "projection_y_coordinate"}
+                ),
+                "x": self.new_x_coord_var(),
+                "y": self.new_y_coord_var(),
+            }
+        )
+        gm = find_grid_mapping_for_var(ds, "sst")
+        self.assertIsInstance(gm, GridMapping)
+        self.assertEqual(CRS_UTM_33N, gm.crs)
+        self.assertEqual(("a", "b"), gm.xy_var_names)
+        self.assertEqual(("x", "y"), gm.xy_dim_names)
