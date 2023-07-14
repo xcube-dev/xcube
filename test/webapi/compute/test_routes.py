bfa677cb68cb246b2c691a3ff6c24f8012327123
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2023-07-14 12:01:41 +0200

    More tests

--- test/webapi/compute/test_routes.py
+++ test/webapi/compute/test_routes.py
@@ -18,6 +18,7 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 # DEALINGS IN THE SOFTWARE.
+import time
 
 from ..helpers import RoutesTestCase
 
@@ -25,15 +26,75 @@ from ..helpers import RoutesTestCase
 class ComputeRoutesTest(RoutesTestCase):
 
     def test_fetch_compute_operations(self):
-        result, status = self.fetch_json('/compute/operations')
-        self.assertIsInstance(result, dict)
-        self.assertIsInstance(result.get("operations"), list)
-        self.assertTrue(len(result.get("operations")) > 0)
+        ops, status = self.fetch_json('/compute/operations')
+        self.assertIsInstance(ops, dict)
+        self.assertIsInstance(ops.get("operations"), list)
+        self.assertTrue(len(ops.get("operations")) > 0)
         self.assertEqual(200, status)
 
     def test_fetch_compute_operation(self):
-        result, status = self.fetch_json('/compute/operations/spatial_subset')
-        self.assertIsInstance(result, dict)
-        self.assertIsInstance(result.get("operationId"), str)
-        self.assertEqual('spatial_subset', result.get("operationId"))
+        op, status = self.fetch_json('/compute/operations/spatial_subset')
+        self.assertIsInstance(op, dict)
+        self.assertIsInstance(op.get("operationId"), str)
+        self.assertEqual('spatial_subset', op.get("operationId"))
         self.assertEqual(200, status)
+
+    def test_fetch_job_management(self):
+        job, status = self.fetch_json(
+            '/compute/jobs',
+            method="PUT",
+            body={
+                "operationId": "spatial_subset",
+                "parameters": {
+                    "dataset": "demo",
+                    "bbox": [1, 51, 4, 52]
+                },
+                "output": {
+                    "datasetId": "demo_subset",
+                    "title": "My demo subset"
+                }
+            }
+        )
+        self.assert_job_ok(job)
+        self.assertEqual(200, status)
+
+        jobs, status = self.fetch_json(f'/compute/jobs')
+        self.assertIsInstance(jobs, dict)
+        self.assertIsInstance(jobs.get("jobs"), list)
+        self.assertEqual(200, status)
+
+        job_id = job.get("jobId")
+        while True:
+            time.sleep(0.1)
+            job, status = self.fetch_json(f'/compute/jobs/{job_id}')
+            self.assert_job_ok(job)
+            job_status = job["state"]["status"]
+            self.assertEqual(200, status)
+
+            if job_status == "started":
+                continue
+
+            if job_status == "completed":
+                # Success!
+                result = job.get("result")
+                self.assertIsInstance(result, dict)
+                self.assertEqual("demo_subset", result.get("datasetId"))
+
+                # Check we can find the result:
+                dataset, status = self.fetch_json('/datasets/demo_subset')
+                self.assertIsInstance(dataset, dict)
+                self.assertEqual(200, status)
+                break
+
+            if job_status != "running":
+                # Fails intentionally
+                self.assertEqual("running", job_status)
+                break
+
+
+    def assert_job_ok(self, job):
+        self.assertIsInstance(job, dict)
+        self.assertIsInstance(job.get("jobId"), int)
+        self.assertIsInstance(job.get("request"), dict)
+        self.assertIsInstance(job.get("state"), dict)
+        self.assertIsInstance(job.get("state").get("status"), str)
