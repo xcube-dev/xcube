7a9b27b14f2095cf63e769095222d2f5d907a55e
Pontus Lurcock <pontus.lurcock@brockmann-consult.de>
2023-08-17 14:26:55 +0200

    Coverages: add stub of web API
    This commit adds the bare minimum to implement a new xcube web API,
    with a single endpoint implementing a minimal demo operation. No actual
    functionality yet.
    
    Initially I'm adding the endpoints under /catalog in order to have them
    under the same prefix as our STAC API; we should probably rename
    /catalog to something like /ows or /ogcapi at a later date.

--- xcube/plugin.py
+++ xcube/plugin.py
@@ -229,6 +229,7 @@ def _register_server_apis(ext_registry: extension.ExtensionRegistry):
         'tiles',
         'timeseries',
         'volumes',
+        'ows.coverages',
         'ows.stac',
         'ows.wmts',
         's3',
