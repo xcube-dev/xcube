4879864070b07f467abd67b1d78c2d1c46048ea8
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2023-10-10 12:18:29 +0200

    only ask s3 file systems for anon property

--- xcube/core/store/fs/impl/dataset.py
+++ xcube/core/store/fs/impl/dataset.py
@@ -376,8 +376,8 @@ class DatasetGeoTiffFsDataAccessor(DatasetFsDataAccessor):
 
     @classmethod
     def create_env_session(cls, fs):
-        aws_unsigned = bool(fs.anon)
         if isinstance(fs, s3fs.S3FileSystem):
+            aws_unsigned = bool(fs.anon)
             aws_session = AWSSession(
                 aws_unsigned=aws_unsigned,
                 aws_secret_access_key=fs.secret,
@@ -389,10 +389,9 @@ class DatasetGeoTiffFsDataAccessor(DatasetFsDataAccessor):
                 ),
             )
             return rasterio.env.Env(session=aws_session,
-                                    aws_no_sign_request=bool(fs.anon)
+                                    aws_no_sign_request=aws_unsigned
                                     )
-        else:
-            return rasterio.env.NullContextManager()
+        return rasterio.env.NullContextManager()
 
     @classmethod
     def open_dataset_with_rioxarray(cls, file_path, overview_level,
