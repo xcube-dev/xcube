630ab775e5d534a7bb2406c4f0b240e3bd24c180
konstntokas <konstantin.ntokas@brockmann-consult.de>
2024-06-21 16:13:14 +0200

    address normans review

--- xcube/core/maskset.py
+++ xcube/core/maskset.py
@@ -205,8 +205,8 @@ class MaskSet:
                 if the flag values are not in the range [0, 2**16 - 1).
 
         Returns:
-            An suitable instance of ```matplotlib.colors.Colormap``` and
-            the corresponding ```matplotlib.colors.BoundaryNorm``` if applicable
+            An suitable instance of ``matplotlib.colors.Colormap`` and
+            the corresponding ``matplotlib.colors.BoundaryNorm`` if applicable
         """
         if self._flag_values is not None:
             flag_var = self._flag_var
