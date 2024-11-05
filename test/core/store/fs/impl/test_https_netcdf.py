e9c86ab93bedaa31dff7b2226f217bc86662bc3f
konstntokas <konstantin.ntokas@brockmann-consult.de>
2024-11-05 14:48:47 +0100

    unittest as mock

--- test/core/store/fs/impl/test_https_netcdf.py
+++ test/core/store/fs/impl/test_https_netcdf.py
@@ -2,42 +2,38 @@
 # Permissions are hereby granted under the terms of the MIT License:
 # https://opensource.org/licenses/MIT.
 
-import os.path
 import unittest
+from unittest.mock import patch
 
-import fsspec
-import rasterio as rio
-import rioxarray
-import s3fs
-import xarray
 import xarray as xr
+import numpy as np
 
 from xcube.core.store import new_data_store
 
 
 class HttpsNetcdfTest(unittest.TestCase):
     """
-    This class tests a lazy access of a NetCDF file from a remote HTTPS server.
+    This class tests the access of a NetCDF file from a remote HTTPS server.
     """
 
-    def test_open_netcdf_https(self):
-        """This test loads GAMIv2-0_2010-2020_100m.nc (217GB), which is available via
-        https://datapub.gfz-potsdam.de/download/10.5880.GFZ.1.4.2023.006-VEnuo/
-        """
-        fs_path = "download/10.5880.GFZ.1.4.2023.006-VEnuo/GAMIv2-0_2010-2020_100m.nc"
-        store = new_data_store("https", root="datapub.gfz-potsdam.de")
-        ds = store.open_data(fs_path, chunks={})
-        self.assertIsInstance(ds, xr.Dataset)
-        self.assertEqual(
-            {"members": 20, "latitude": 202500, "longitude": 405000, "time": 2},
-            ds.sizes,
+    @patch("xarray.open_dataset")
+    def test_open_netcdf_https(self, mock_open_dataset):
+        # set-up mock
+        mock_data = {
+            "temperature": (("time", "x", "y"), np.random.rand(5, 5, 5)),
+            "precipitation": (("time", "x", "y"), np.random.rand(5, 5, 5)),
+        }
+        mock_ds = xr.Dataset(mock_data)
+        mock_open_dataset.return_value = mock_ds
+
+        fs_path = "mockfile.nc"
+        store = new_data_store("https", root="root.de")
+        ds = store.open_data(fs_path)
+
+        mock_open_dataset.assert_called_once_with(
+            "https://root.de/mockfile.nc#mode=bytes", engine="netcdf4"
         )
-        self.assertEqual(
-            [1, 7789, 15577, 1],
-            [
-                ds.chunksizes["members"][0],
-                ds.chunksizes["latitude"][0],
-                ds.chunksizes["longitude"][0],
-                ds.chunksizes["time"][0],
-            ],
-        )
+        self.assertTrue("temperature" in ds)
+        self.assertTrue("precipitation" in ds)
+        self.assertEqual(ds["temperature"].shape, (5, 5, 5))
+        self.assertEqual(ds["precipitation"].shape, (5, 5, 5))
