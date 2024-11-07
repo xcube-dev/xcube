d5003d6dc0da204ff82d73903e6ec14f374cab64
Norman Fomferra <norman.fomferra@gmail.com>
2024-11-07 19:33:16 +0100

    Initial demo code for viewer extensions

---
+++ examples/serve/demo3/my_viewer_ext/my_panel_1.py
@@ -0,0 +1,77 @@
+from dashipy import Component, Input, State, Output
+from dashipy.components import Box, Dropdown, Checkbox, Typography
+from dashipy.demo.contribs import Panel
+from dashipy.demo.context import Context
+
+
+panel = Panel(__name__, title="Panel B")
+
+
+COLORS = [("red", 0), ("green", 1), ("blue", 2), ("yellow", 3)]
+
+
+@panel.layout(
+    Input(source="app", property="control.selectedDatasetId"),
+)
+def render_panel(
+    ctx: Context,
+    dataset_id: str = "",
+) -> Component:
+
+    opaque = False
+    color = 0
+
+    opaque_checkbox = Checkbox(
+        id="opaque",
+        value=opaque,
+        label="Opaque",
+    )
+
+    color_dropdown = Dropdown(
+        id="color",
+        value=color,
+        label="Color",
+        options=COLORS,
+        style={"flexGrow": 0, "minWidth": 80},
+    )
+
+    info_text = Typography(
+        id="info_text", text=update_info_text(ctx, dataset_id, opaque, color)
+    )
+
+    return Box(
+        style={
+            "display": "flex",
+            "flexDirection": "column",
+            "width": "100%",
+            "height": "100%",
+            "gap": "6px",
+        },
+        children=[opaque_checkbox, color_dropdown, info_text],
+    )
+
+
+# noinspection PyUnusedLocal
+@panel.callback(
+    Input(source="app", property="control.selectedDatasetId"),
+    Input("opaque"),
+    Input("color"),
+    State("info_text", "text"),
+    Output("info_text", "text"),
+)
+def update_info_text(
+    ctx: Context,
+    dataset_id: str = "",
+    opaque: bool = False,
+    color: int = 0,
+    info_text: str = "",
+) -> str:
+    opaque = opaque or False
+    color = color if color is not None else 0
+    return (
+        f"The dataset is {dataset_id},"
+        f" the color is {COLORS[color][0]} and"
+        f" it {'is' if opaque else 'is not'} opaque."
+        f" The length of the last info text"
+        f" was {len(info_text or "")}."
+    )
