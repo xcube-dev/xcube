a160a1261d719084e24e5ea4bec9bd1570a5ab5a
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2024-11-12 11:21:23 +0100

    added server-side panels test

---
+++ test/webapi/res/viewer_panels/__init__.py
@@ -0,0 +1,7 @@
+from dashipy import Extension
+from .my_panel_a import panel as my_panel_a
+from .my_panel_b import panel as my_panel_b
+
+ext = Extension(__name__)
+ext.add(my_panel_a)
+ext.add(my_panel_b)
