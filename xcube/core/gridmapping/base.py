a94b389dbda69503760e0200e2807c445641098c
konstntokas <konstantin.ntokas@brockmann-consult.de>
2024-11-13 11:49:50 +0100

    error in doc-string corrected

--- xcube/core/gridmapping/base.py
+++ xcube/core/gridmapping/base.py
@@ -206,10 +206,10 @@ class GridMapping(abc.ABC):
     ) -> "GridMapping":
         """Derive a scaled version of this regular grid mapping.
 
-        Scaling factors lower than one correspond to up-scaling
+        Scaling factors larger than one correspond to up-scaling
         (pixels sizes decrease, image size increases).
 
-        Scaling factors larger than one correspond to down-scaling.
+        Scaling factors lower than one correspond to down-scaling.
         (pixels sizes increase, image size decreases).
 
         Args:
@@ -729,7 +729,7 @@ class GridMapping(abc.ABC):
         Args:
             crs: The new spatial coordinate reference system.
             xy_res: Optional resolution in x- and y-directions.
-                If given, speeds up the method by avoiding time-consuming 
+                If given, speeds up the method by avoiding time-consuming
                 spatial resolution estimation.
             tile_size: Optional new tile size.
             xy_var_names: Optional new coordinate names.
