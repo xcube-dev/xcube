a0a97165c614af6466ab38d369194d1261d00cbb
Norman Fomferra <norman.fomferra@gmail.com>
2024-11-20 17:10:10 +0100

    Made it work, but it is ugly still

--- examples/serve/panels-demo/my_viewer_ext/my_panel_1.py
+++ examples/serve/panels-demo/my_viewer_ext/my_panel_1.py
@@ -1,16 +1,27 @@
 import altair as alt
-from chartlets import Component, Input, State, Output
-from chartlets.components import Box, Button, Plot, Select
 import numpy as np
 import pandas as pd
+import pyproj
+import shapely
+import shapely.ops
+from chartlets import Component, Input, State, Output
+from chartlets.components import Box, Button, Plot, Select
 
-from xcube.core.geom import mask_dataset_by_geometry
+from xcube.constants import CRS_CRS84
+from xcube.core.geom import mask_dataset_by_geometry, normalize_geometry
+from xcube.core.gridmapping import GridMapping
 from xcube.webapi.viewer.contrib import Panel
 from xcube.webapi.viewer.contrib import get_dataset
 from xcube.server.api import Context
 
 
 panel = Panel(__name__, title="2D Histogram")
+
+
+# Number of bins in x and y directions.
+# This results in columns of 4096 items.
+# Vega Altair's maximum is 5000.
+NUM_BINS_MAX = 64
 
 
 @panel.layout(State(source="app", property="controlState.selectedDatasetId"))
@@ -38,20 +49,20 @@ def render_panel(ctx: Context, dataset_id: str | None = None) -> Component:
         id="select_var_2", label="Variable 2", value=var_name_2, options=var_names
     )
 
-    select_box = Box(
-        children=[select_var_1, select_var_2],
+    button = Button(id="button", text="Update", style={"maxWidth": 100})
+
+    controls = Box(
+        children=[select_var_1, select_var_2, button],
         style={
             "display": "flex",
             "flexDirection": "row",
             "gap": 6,
-            "paddingTop": 40,
+            "paddingTop": 50,
         },
     )
 
-    button = Button(id="button", text="Update", disabled=True, style={"maxWidth": 100})
-
     return Box(
-        children=[plot, select_box, button],
+        children=[plot, controls],
         style={
             "display": "flex",
             "flexDirection": "column",
@@ -87,45 +98,65 @@ def update_plot(
     if dataset is None or place_geometry is None or not var_name_1 or not var_name_2:
         print("panel disabled")
         return None
+
     if "time" in dataset.coords:
         if time_label:
             dataset = dataset.sel(time=pd.Timestamp(time_label[0:-1]), method="nearest")
         else:
             dataset = dataset.isel(time=-1)
 
+    grid_mapping = GridMapping.from_dataset(dataset)
+    place_geometry = normalize_geometry(place_geometry)
+    if place_geometry is not None and not grid_mapping.crs.is_geographic:
+        project = pyproj.Transformer.from_crs(
+            CRS_CRS84, grid_mapping.crs, always_xy=True
+        ).transform
+        place_geometry = shapely.ops.transform(project, place_geometry)
+
     dataset = mask_dataset_by_geometry(dataset, place_geometry)
+    if dataset is None:
+        print("dataset is None after masking, invalid geometry?")
+        return None
 
-    var_data_1: np.ndarray = dataset[var_name_1].values.flatten()
-    var_data_2: np.ndarray = dataset[var_name_2].values.flatten()
-    x_range = [np.nanmin(var_data_1), np.nanmax(var_data_1)]
-    y_range = [np.nanmin(var_data_2), np.nanmax(var_data_2)]
-    num_bins = min(64, var_data_1.size)
+    var_1_data: np.ndarray = dataset[var_name_1].values.ravel()
+    var_2_data: np.ndarray = dataset[var_name_2].values.ravel()
+    var_1_range = [np.nanmin(var_1_data), np.nanmax(var_1_data)]
+    var_2_range = [np.nanmin(var_2_data), np.nanmax(var_2_data)]
+    num_bins = min(NUM_BINS_MAX, var_1_data.size)
     hist2d, x_edges, y_edges = np.histogram2d(
-        var_data_1,
-        var_data_2,
+        var_1_data,
+        var_2_data,
         bins=num_bins,
-        range=np.array([x_range, y_range]),
+        range=np.array([var_1_range, var_2_range]),
+        density=True,
     )
-    print(np.nanmin(hist2d), np.nanmax(hist2d))
-
-    x, y = np.meshgrid(
-        np.linspace(x_range[0], x_range[1], num_bins),
-        np.linspace(y_range[0], y_range[1], num_bins),
-    )
-
+    # x and y 2D arrays with bin centers
+    x, y = np.meshgrid(np.arange(num_bins), np.arange(num_bins)[::-1])
+    # z = hist2d
+    z = np.where(hist2d > 0.0, hist2d, np.nan)
     source = pd.DataFrame(
-        {var_name_1: x.ravel(), var_name_2: y.ravel(), "count": hist2d.ravel()}
+        {var_name_1: x.ravel(), var_name_2: y.ravel(), "z": z.ravel()}
     )
-
+    # TODO: use edges or center coordinates as tick labels.
+    x_centers = x_edges[0:-1] + np.diff(x_edges) / 2
+    y_centers = y_edges[0:-1] + np.diff(y_edges) / 2
+    # TODO: limit number of ticks on axes to, e.g., 10.
+    # TODO: allow chart to be adjusted to available container (<div>) size.
     return (
         alt.Chart(source)
         .mark_rect()
         .encode(
-            x=f"{var_name_1}:Q",
-            y=f"{var_name_2}:Q",
-            color=alt.Color("count:Q", scale=alt.Scale(scheme="greenblue")),
+            x=alt.X(
+                f"{var_name_1}:O",
+                # scale=alt.Scale(bins=x_centers),
+            ),
+            y=alt.Y(
+                f"{var_name_2}:O",
+                # scale=alt.Scale(bins=y_centers),
+            ),
+            color=alt.Color("z:Q", scale=alt.Scale(scheme="viridis"), title="Density"),
         )
-    )
+    ).properties(width=320, height=320)
 
 
 @panel.callback(
@@ -134,12 +165,11 @@ def update_plot(
     Output("button", "disabled"),
 )
 def enable_button(
-    ctx: Context,
+    _ctx: Context,
     dataset_id: str | None = None,
     place_geometry: str | None = None,
 ) -> bool:
-    dataset = get_dataset(ctx, dataset_id)
-    return dataset is None or place_geometry is None
+    return not dataset_id or not place_geometry
 
 
 @panel.callback(
