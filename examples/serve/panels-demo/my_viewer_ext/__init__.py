0f7b731899c45e989375e1a33b176a9cd7a9b44a
clarasb <clara.backens@web.de>
2025-03-11 12:31:57 +0100

    rename demo file

--- examples/serve/panels-demo/my_viewer_ext/__init__.py
+++ examples/serve/panels-demo/my_viewer_ext/__init__.py
@@ -3,10 +3,10 @@ from .my_panel_1 import panel as my_panel_1
 
 # from .my_panel_2 import panel as my_panel_2
 
-from .my_panel_4 import panel as my_panel_4
+from .my_panel_3 import panel as my_panel_3
 
 ext = Extension(__name__)
 ext.add(my_panel_1)
 # ext.add(my_panel_2)
 
-ext.add(my_panel_4)
+ext.add(my_panel_3)
