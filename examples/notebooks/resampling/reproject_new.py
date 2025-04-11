46896fa5dbd074d6471419032ce641c6f2a26864
konstntokas <konstantin.ntokas@brockmann-consult.de>
2025-04-11 17:22:41 +0200

    first draft works

---
+++ examples/notebooks/resampling/reproject_new.py
@@ -0,0 +1,42 @@
+from xcube.core.store import new_data_store
+from xcube.core.resampling.reproject import reproject_dataset
+from xcube.core.gridmapping import GridMapping
+from datetime import datetime
+import matplotlib.pyplot as plt
+import pyproj
+
+
+store = new_data_store("s3", root="deep-esdl-public")
+mlds_lc = store.open_data("LC-1x2025x2025-2.0.0.levels")
+ds = mlds_lc.base_dataset
+
+ds = ds.sel(time=slice(datetime(2018, 1, 1), datetime(2022, 1, 1)))
+ds = ds[["lccs_class"]]
+print(ds)
+
+bbox = [-20, 25, 40, 85]
+target_crs = "EPSG:3035"
+t = pyproj.Transformer.from_crs("EPSG:4326", target_crs, always_xy=True)
+target_bbox = t.transform_bounds(*bbox)
+spatial_res = 300
+x_size = int((target_bbox[2] - target_bbox[0]) / spatial_res) + 1
+y_size = int(abs(target_bbox[3] - target_bbox[1]) / spatial_res) + 1
+target_gm = GridMapping.regular(
+    size=(x_size, y_size),
+    xy_min=(target_bbox[0] - spatial_res / 2, target_bbox[1] - spatial_res / 2),
+    xy_res=spatial_res,
+    crs=target_crs,
+    tile_size=1000,
+)
+
+start = datetime.now()
+ds_reproject = reproject_dataset(ds, target_gm=target_gm)
+elapsed = (datetime.now() - start).total_seconds()
+print(f"Computational time reproject_dataset: {elapsed:.2f}sec")
+print(ds_reproject)
+
+start = datetime.now()
+ds_reproject.lccs_class.isel(time=-1)[::10, ::10].plot()
+elapsed = (datetime.now() - start).total_seconds()
+print(f"Computational time plot: {elapsed:.2f}sec")
+plt.show()
