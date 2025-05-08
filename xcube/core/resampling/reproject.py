18ceb581c23eb67778092e698d68117c9c12dff0
konstntokas <konstantin.ntokas@brockmann-consult.de>
2025-05-08 17:20:21 +0200

    interpoation done

--- xcube/core/resampling/reproject.py
+++ xcube/core/resampling/reproject.py
@@ -25,6 +25,61 @@ def reproject_dataset(
     fill_value: int | float | None = None,
     interpolation: int = 0,
 ):
+    """This function reprojects a dataset *source_ds*
+    to another projection defined by *target_gm* or derived by *ref_ds*.
+
+    The function expects *source_ds* or the given
+    *source_gm* to have a two-dimensional
+    coordinate variables that provide spatial x,y coordinates
+    for every data variable with the same spatial dimensions.
+
+    For example, a dataset may comprise variables with
+    spatial dimensions ``var(..., y_dim, x_dim)``,
+    then the function expects coordinates to be provided
+    in two forms:
+
+    2. Two-dimensional ``x_var(y_dim, x_dim)``
+       and ``y_var(y_dim, x_dim)`` (coordinate) variables.
+
+    If *target_gm* is given, and it defines a tile size,
+    or *tile_size* is given and the number of tiles is
+    greater than one in the output's x- or y-direction, then the
+    returned dataset will be composed of lazy, chunked dask
+    arrays. Otherwise, the returned dataset will be composed
+    of ordinary numpy arrays.
+
+    Args:
+        source_ds: Source dataset.
+        source_gm: Source dataset grid mapping.
+        target_gm: Optional target geometry. If not given, output
+            geometry will be computed to spatially fit *dataset* and to
+            retain its spatial resolution.
+        ref_ds: An optional dataset that provides the
+            target grid mapping if *target_gm* is not provided.
+            If *ref_ds* is given, its coordinate variables are copied
+            by reference into the returned dataset.
+        fill_value: fill value to be used for exceeding edges; if None, default
+            values are taken defined by the data type. Fill value for float datasets
+            is set to `np.nan`, fill value for uint8 is set to 255, fill value for
+            uint16 is set to 65535, for all remaining integer datasets fill value
+            is set to -1.
+        interpolation: Interpolation method for computing output pixels.
+            If given, must be "nearest", "triangular", or "bilinear".
+            The default is "nearest". The "triangular" interpolation is
+            performed between 3 and "bilinear" between 4 adjacent source
+            pixels. Both are applied only to variables of
+            floating point type. If you need to interpolate between
+            integer data you should cast it to float first.
+
+    Returns:
+        A reprojected dataset, or None if the requested output does not
+        intersect with *dataset*.
+    """
+    # TODO: treat 2d and 3d case
+    # TODO: select interpolation based on dtype
+    # TODO: test interpolation
+    # TODO: write tests
+    # TODO: write comments for code understadning
     if source_gm is None:
         source_gm = GridMapping.from_dataset(source_ds)
 
@@ -38,11 +93,11 @@ def reproject_dataset(
     )
 
     # get indices for each box in source dataset
-    scr_ij_bboxes, x_coords, y_coords = _get_scr_bboxes_indices(
+    scr_ij_bboxes, x_coords, y_coords, pad_width = _get_scr_bboxes_indices(
         transformer, source_gm, target_gm
     )
 
-    # transform grid points from traget grid mapping to source grid mapping
+    # transform grid points from target grid mapping to source grid mapping
     source_xx, source_yy = _transform_gridpoints(transformer, target_gm)
 
     # reproject dataset
@@ -60,30 +115,31 @@ def reproject_dataset(
     for var_name, data_array in source_ds.items():
         if data_array.dims[-2:] != xy_dims:
             continue
+        # reorganize data array slice to align with the
+        # chunks of source_xx and source_yy
+        scr_data = _reorganize_data_array_slice(
+            data_array,
+            x_coords,
+            y_coords,
+            scr_ij_bboxes,
+            pad_width,
+            fill_value,
+        )
         slices_reprojected = []
         for idx, chunk_size in enumerate(data_array.chunks[0]):
-            # reorganize data array slice to align with the
-            # chunks of source_xx and source_yy
-            scr_data = _reorganize_data_array_slice(
-                data_array,
-                x_coords,
-                y_coords,
-                idx,
-                chunk_size,
-                scr_ij_bboxes,
-                fill_value,
-            )
+            dim0_start = idx * chunk_size
+            dim0_end = (idx + 1) * chunk_size
 
             data_reprojected = da.map_blocks(
                 _reproject_block,
                 source_xx,
                 source_yy,
-                scr_data,
+                scr_data[dim0_start:dim0_end],
                 x_coords,
                 y_coords,
                 dtype=data_array.dtype,
                 chunks=(
-                    scr_data.shape[0],
+                    scr_data[dim0_start:dim0_end].shape[0],
                     source_yy.chunks[0][0],
                     source_yy.chunks[1][0],
                 ),
@@ -116,11 +172,54 @@ def _reproject_block(
     iy = (source_yy - y_coord[0]) / -scr_y_res
 
     if interpolation == 0:
+        # interpolation == "nearest"
         ix = np.rint(ix).astype(np.int16)
         iy = np.rint(iy).astype(np.int16)
         data_reprojected = scr_data[:, iy, ix]
+    elif interpolation == 1:
+        # interpolation == "triangular"
+        ix_ceil = np.ceil(ix).astype(np.int16)
+        ix_floor = np.floor(ix).astype(np.int16)
+        iy_ceil = np.ceil(iy).astype(np.int16)
+        iy_floor = np.floor(iy).astype(np.int16)
+        diff_ix = ix - ix_floor
+        diff_iy = iy - iy_floor
+        value_00 = scr_data[:, iy_floor, ix_floor]
+        value_01 = scr_data[:, iy_floor, ix_ceil]
+        value_10 = scr_data[:, iy_ceil, ix_floor]
+        value_11 = scr_data[:, iy_ceil, ix_ceil]
+        mask = diff_ix + diff_iy < 1.0
+        mask = np.repeat(mask[np.newaxis, :, :], scr_data.shape[0], axis=0)
+        data_reprojected = np.zeros(
+            (scr_data.shape[0], iy.shape[0], iy.shape[1]), dtype=scr_data.dtype
+        )
+        # Closest triangle
+        data_reprojected[mask] = (
+            value_00[mask]
+            + diff_ix[mask] * (value_01[mask] - value_00[mask])
+            + diff_iy[mask] * (value_10[mask] - value_00[mask])
+        )
+        # Opposite triangle
+        data_reprojected[~mask] = (
+            value_11[~mask]
+            + (1.0 - diff_ix[~mask]) * (value_10[~mask] - value_11[~mask])
+            + (1.0 - diff_iy[~mask]) * (value_01[~mask] - value_11[~mask])
+        )
     else:
-        raise NotImplementedError()
+        # interpolation == "bilinear"
+        ix_ceil = np.ceil(ix).astype(np.int16)
+        ix_floor = np.floor(ix).astype(np.int16)
+        iy_ceil = np.ceil(iy).astype(np.int16)
+        iy_floor = np.floor(iy).astype(np.int16)
+        diff_ix = ix - ix_floor
+        diff_iy = iy - iy_floor
+        value_00 = scr_data[:, iy_floor, ix_floor]
+        value_01 = scr_data[:, iy_floor, ix_ceil]
+        value_10 = scr_data[:, iy_ceil, ix_floor]
+        value_11 = scr_data[:, iy_ceil, ix_ceil]
+        value_u0 = value_00 + diff_ix * (value_01 - value_00)
+        value_u1 = value_10 + diff_ix * (value_11 - value_10)
+        data_reprojected = value_u0 + diff_iy * (value_u1 - value_u0)
 
     return data_reprojected
 
@@ -199,7 +298,21 @@ def _get_scr_bboxes_indices(
     x_coords = da.from_array(x_coords, chunks=(-1, 1, 1))
     y_coords = da.from_array(y_coords, chunks=(-1, 1, 1))
 
-    return scr_ij_bboxes, x_coords, y_coords
+    pad_width = (
+        (0, 0),
+        (
+            -min(0, int(j_min)),
+            max(0, int(j_max - source_gm.height)),
+        ),
+        (
+            -min(0, int(i_min)),
+            max(0, int(i_max - source_gm.width)),
+        ),
+    )
+    scr_ij_bboxes[[1, 3]] += pad_width[1][0]
+    scr_ij_bboxes[[0, 2]] += pad_width[2][0]
+
+    return scr_ij_bboxes, x_coords, y_coords, pad_width
 
 
 def _transform_gridpoints(
@@ -248,105 +361,35 @@ def _reorganize_data_array_slice(
     data_array: xr.DataArray,
     x_coords: da.Array,
     y_coords: da.Array,
-    idx: int,
-    chunk_size_dim0: int,
     scr_ij_bboxes: np.ndarray,
+    pad_width: tuple[tuple[int]],
     fill_value: int | float | None,
 ) -> da.Array:
     fill_value_da = _get_fill_value(data_array, fill_value)
 
-    dim0_start = idx * chunk_size_dim0
-    dim0_end = (idx + 1) * chunk_size_dim0
-
-    scr_data = da.zeros(
+    data_out = da.zeros(
         (
-            dim0_end - dim0_start,
+            data_array.data.shape[0],
             y_coords.shape[0] * scr_ij_bboxes.shape[1],
             x_coords.shape[0] * scr_ij_bboxes.shape[2],
         ),
-        chunks=(-1, y_coords.shape[0], x_coords.shape[0]),
+        chunks=(data_array.chunks[0][0], y_coords.shape[0], x_coords.shape[0]),
         dtype=data_array.dtype,
     )
-
+    data_in = da.pad(
+        data_array.data, pad_width, mode="constant", constant_values=fill_value_da
+    )
     for i in range(scr_ij_bboxes.shape[2]):
         for j in range(scr_ij_bboxes.shape[1]):
             scr_ij_bbox = scr_ij_bboxes[:, j, i]
-            if _target_chunk_fully_intersects_source(scr_ij_bbox, data_array):
-                scr_data[
-                    :,
-                    j * y_coords.shape[0] : (j + 1) * y_coords.shape[0],
-                    i * x_coords.shape[0] : (i + 1) * x_coords.shape[0],
-                ] = data_array.data[
-                    dim0_start:dim0_end,
-                    scr_ij_bbox[1] : scr_ij_bbox[3],
-                    scr_ij_bbox[0] : scr_ij_bbox[2],
-                ]
-            elif _target_chunk_not_intersects_source(scr_ij_bbox, data_array):
-                scr_data[
-                    :,
-                    j * y_coords.shape[0] : (j + 1) * y_coords.shape[0],
-                    i * x_coords.shape[0] : (i + 1) * x_coords.shape[0],
-                ] = da.full(
-                    (
-                        dim0_end - dim0_start,
-                        y_coords.shape[0],
-                        x_coords.shape[0],
-                    ),
-                    fill_value_da,
-                    chunks=(-1, -1, -1),
-                    dtype=data_array.dtype,
-                )
-            else:
-                scr_ij_bbox_mod = [
-                    max(0, int(scr_ij_bbox[0])),
-                    max(0, int(scr_ij_bbox[1])),
-                    min(data_array.shape[2], int(scr_ij_bbox[2])),
-                    min(data_array.shape[1], int(scr_ij_bbox[3])),
-                ]
-                data = data_array.data[
-                    dim0_start:dim0_end,
-                    scr_ij_bbox_mod[1] : scr_ij_bbox_mod[3],
-                    scr_ij_bbox_mod[0] : scr_ij_bbox_mod[2],
-                ]
-                pad_width = (
-                    (0, 0),
-                    (
-                        -min(0, int(scr_ij_bbox[1])),
-                        max(0, int(scr_ij_bbox[3] - data_array.shape[1])),
-                    ),
-                    (
-                        -min(0, int(scr_ij_bbox[0])),
-                        max(0, int(scr_ij_bbox[2] - data_array.shape[2])),
-                    ),
-                )
-                scr_data[
-                    :,
-                    j * y_coords.shape[0] : (j + 1) * y_coords.shape[0],
-                    i * x_coords.shape[0] : (i + 1) * x_coords.shape[0],
-                ] = da.pad(
-                    data, pad_width, mode="constant", constant_values=fill_value_da
-                )
+            data_out[
+                :,
+                j * y_coords.shape[0] : (j + 1) * y_coords.shape[0],
+                i * x_coords.shape[0] : (i + 1) * x_coords.shape[0],
+            ] = data_in[
+                :,
+                scr_ij_bbox[1] : scr_ij_bbox[3],
+                scr_ij_bbox[0] : scr_ij_bbox[2],
+            ]
 
-    return scr_data
-
-
-def _target_chunk_not_intersects_source(
-    scr_ij_bbox: np.ndarray, data_array: xr.DataArray
-):
-    return (
-        scr_ij_bbox[2] < 0
-        or scr_ij_bbox[0] >= data_array.data.shape[2]
-        or scr_ij_bbox[3] < 0
-        or scr_ij_bbox[1] >= data_array.data.shape[1]
-    )
-
-
-def _target_chunk_fully_intersects_source(
-    scr_ij_bbox: np.ndarray, data_array: xr.DataArray
-):
-    return (
-        scr_ij_bbox[0] >= 0
-        and scr_ij_bbox[2] < data_array.data.shape[2]
-        and scr_ij_bbox[1] >= 0
-        and scr_ij_bbox[3] < data_array.data.shape[1]
-    )
+    return data_out
