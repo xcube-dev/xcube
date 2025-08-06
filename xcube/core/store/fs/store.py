e2e54a009e2d946993694932011d49abdd2eaa73
TonioF <tonio.fincke@brockmann-consult.de>
2025-08-06 14:24:03 +0200

    got rid of concept of suitability

--- xcube/core/store/fs/store.py
+++ xcube/core/store/fs/store.py
@@ -469,7 +469,7 @@ class BaseFsDataStore(DefaultSearchMixin, MutableDataStore):
             extensions = ext.metadata.get("extensions", [])
             for extension in extensions:
                 filename_ext_to_format[extension] = fmt
-            if ext.metadata.get("suitability", "primary") == "primary":
+            if data_type == "mldataset":
                 format_to_data_type_aliases[fmt] = (
                     (data_type, ) + format_to_data_type_aliases.get(fmt, ())
                 )
