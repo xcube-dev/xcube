4904627e10823f777d273ddb674aca2e5caabaca
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2026-07-27 06:51:14 +0200

    reverted version changes

--- xcube/version.py
+++ xcube/version.py
@@ -2,9 +2,4 @@
 # Permissions are hereby granted under the terms of the MIT License:
 # https://opensource.org/licenses/MIT.
 
-from importlib.metadata import version as get_version, PackageNotFoundError
-
-try:
-    version = get_version("xcube")
-except PackageNotFoundError:
-    version = get_version("xcube-core")
+version = "1.13.4.dev0"
